95dcc95711f32de644729e1e2fae17c186135b2f
Lothar Rubusch <l.rubusch@gmx.ch>
2013-10-22 23:57:29 +0200

    indention

--- Linux_wait/wait.c
+++ Linux_wait/wait.c
@@ -3,13 +3,12 @@
   #include <sys/wait.h>
   pid_t wait(int* status);
 
-  Wait for child process termination. Makes a parent wait until 
-  a child process is completed. BLOCKS calling process until the 
-  child terminates. If the parent has various child processes, 
-  wait() returns when one returns.
+  Wait for child process termination. Makes a parent wait until a child process
+  is completed. BLOCKS calling process until the child terminates. If the parent
+  has various child processes, wait() returns when one returns.
 
   status: the termination status of the process, can be NULL.
-  Returns pid of terminated child on success, else -1  
+  Returns pid of terminated child on success, else -1
 //*/
 
 
@@ -28,53 +27,53 @@
 
 int main()
 {
-  char* identifier=NULL;
-  if(NULL == (identifier = calloc(IDENTIFIER_SIZE, sizeof(*identifier)))){
-    perror("calloc() failed");
-    exit(1);
-  }
-  memset(identifier, '\0', IDENTIFIER_SIZE); 
-  pid_t pid=0, pid_parent=getpid();
- 
-  // fork
-  puts("fork()");
-  if(0 > (pid = fork())){
-    perror("fork() failed");
-    exit(1);
+	char* identifier=NULL;
+	if(NULL == (identifier = calloc(IDENTIFIER_SIZE, sizeof(*identifier)))){
+		perror("calloc() failed");
+		exit(1);
+	}
+	memset(identifier, '\0', IDENTIFIER_SIZE);
+	pid_t pid=0, pid_parent=getpid();
 
-  }else if(pid == 0){
-    // child process
-    strncpy(identifier, CHILD_TXT, strlen(CHILD_TXT));
+	// fork
+	puts("fork()");
+	if(0 > (pid = fork())){
+		perror("fork() failed");
+		exit(1);
 
-    // child code
-    printf("%schild pid: %i, parent: %i\r\n", identifier, getpid(), pid_parent);
-    printf("%ssleeps\r\n", identifier);
-    sleep(5);
-    printf("%sawakes\r\n", identifier);
-    printf("%sdone\r\n", identifier);
-    exit(0);
+	}else if(pid == 0){
+		// child process
+		strncpy(identifier, CHILD_TXT, strlen(CHILD_TXT));
 
-  }else{
-    // parent process
-    strncpy(identifier, PARENT_TXT, strlen(PARENT_TXT));
+		// child code
+		printf("%schild pid: %i, parent: %i\r\n", identifier, getpid(), pid_parent);
+		printf("%ssleeps\r\n", identifier);
+		sleep(5);
+		printf("%sawakes\r\n", identifier);
+		printf("%sdone\r\n", identifier);
+		exit(0);
 
-    printf("%schild pid: %i, parent: %i\r\n", identifier, pid, getpid());
-    printf("%swaiting on pid %i\r\n", identifier, pid);
+	}else{
+		// parent process
+		strncpy(identifier, PARENT_TXT, strlen(PARENT_TXT));
 
-    /*
-      1. child exit status
-    //*/
-    int childExitStatus=0;
+		printf("%schild pid: %i, parent: %i\r\n", identifier, pid, getpid());
+		printf("%swaiting on pid %i\r\n", identifier, pid);
 
-    /*
-      2. wait on all childs
-    //*/
-    while(0 == wait(&childExitStatus))
-      ;
-    printf("%swaiting done\r\n", identifier);
+		/*
+		  1. child exit status
+		//*/
+		int childExitStatus=0;
 
-    printf("%sdone\r\n", identifier);
-    exit(0);
-  }
+		/*
+		  2. wait on all childs
+		//*/
+		while(0 == wait(&childExitStatus))
+			;
+		printf("%swaiting done\r\n", identifier);
+
+		printf("%sdone\r\n", identifier);
+		exit(0);
+	}
 }
 
