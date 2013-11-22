7792ab9a4d0dd9246d5cc7fd0fc54c55a6c3864c
Lothar Rubusch <l.rubusch@gmx.ch>
2013-11-22 02:14:54 +0100

    ptrace, registers

--- ptrace__register/ptrace.c
+++ ptrace__register/ptrace.c
@@ -3,6 +3,10 @@
 
   author: Lothar Rubusch
   email: L.Rubusch@gmx.ch
+
+  using the user_regs_struct to handle register values directly,
+
+  [example based on Linux Journal (Oct 31, 2002 By Pradeep Padala) ]
 */
 
 #include <sys/ptrace.h>
@@ -11,6 +15,7 @@
 #include <unistd.h>
 #include <asm/ptrace-abi.h> /* constants, e.g. ORIG_EAX, etc. */
 #include <sys/syscall.h>
+#include <sys/user.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,6 +25,43 @@ main( int argc, char** argv )
 {
 	pid_t child;
 	long orig_eax, eax;
-	long params[3];
+	int status;
+	int insyscall = 0;
+	struct user_regs_struct regs; /* registers */
 
+	if(0 > (child = fork() )){
+		perror( "fork() failed" );
+	}else if( 0 == child ){
+		/* child */
+		ptrace( PTRACE_TRACEME, 0, NULL, NULL );
+		execl( "/bin/pwd", "pwd", NULL );
+	}else{
+		while( 1 ){
+
+			/* check if ptrace stopped child, or if it exited */
+			wait( &status );
+			if( WIFEXITED(status) ){
+				break;
+			}
+
+			orig_eax = ptrace( PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL );
+			if( orig_eax == SYS_write ){
+				if( insyscall == 0 ){
+					/* syscall entry */
+					insyscall = 1;
+
+					/* get registers */
+					ptrace( PTRACE_GETREGS, child, NULL, &regs );
+					fprintf( stderr, "parent: write called with %lu, %lu, %lu\n", regs.ebx, regs.ecx, regs.edx );
+				}else{
+					/* syscall exit */
+					eax = ptrace( PTRACE_PEEKUSER, child, 4 * EAX, NULL );
+					fprintf( stderr, "parent: write returned with %lu\n", eax );
+					insyscall = 0;
+				}
+			}
+			ptrace( PTRACE_SYSCALL, child, NULL, NULL );
+		}
+	}
+	return 0;
 }
