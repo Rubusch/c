9a233615c47c7daf133ca8ac03fc6f04e4f64ca4
Lothar Rubusch <l.rubusch@gmx.ch>
2013-11-27 15:01:55 +0100

    working on the project

--- ptrace04__revertstring_FIXME/ptrace.c
+++ ptrace04__revertstring_FIXME/ptrace.c
@@ -20,21 +20,9 @@
 #include <stdio.h>
 #include <string.h>
 
-// FIXME, comments
+// TODO comments
 
 const int long_size = sizeof( long );
-
-void
-reverse( char *str )
-{
-	int i, j;
-	char temp;
-	for( i=0, j=strlen(str)-2; i<=j; ++i,--j){
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-	}
-}
 
 void
 getdata( pid_t child, long addr, char* str, int len )
@@ -46,21 +34,43 @@ getdata( pid_t child, long addr, char* str, int len )
 		char chars[long_size];
 	} data;
 
-	i=0;
-	j=len/long_size;
+	i = 0;
+	j = len / long_size;
 	laddr=str;
 	while( i < j ){
+		/* get data 'val' */
 		data.val = ptrace( PTRACE_PEEKDATA, child, addr + i * 4, NULL );
+
+		/* copy over 'chars' */
 		memcpy( laddr, data.chars, long_size );
+
+		/* increment read index */
 		++i;
+
+		/* increment write position - move pointer */
 		laddr+=long_size;
 	}
-	j=len%long_size;
-	if(j!=0){
+// TODO
+	j = len % long_size;
+	if( j!=0 ){
 		data.val = ptrace( PTRACE_PEEKDATA, child, addr + i * 4, NULL );
 		memcpy( laddr, data.chars, j );
 	}
 	str[len] = '\0';
+}
+
+void
+reverse( char *str )
+{
+	int i, j;
+	char temp;
+	for( i=0, j=strlen(str)-2; i<=j; ++i,--j){
+		/* swap */
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
+//	str[strlen(str)-1] = "\n"; // TODO
 }
 
 void
@@ -77,14 +87,26 @@ putdata( pid_t child, long addr, char* str, int len )
 	j=len/long_size;
 	laddr = str;
 	while( i < j ){
+		/* copy 'chars' */
 		memcpy( data.chars, laddr, long_size );
+
+		/* write to val */
 		ptrace( PTRACE_POKEDATA, child, addr + i * 4, data.val );
+
+		/* next to read */
 		++i;
+
+		/* next to write */
 		laddr += long_size;
 	}
+
 	j = len % long_size;
+
 	if( j != 0 ){
+		/* copy value to 'chars' */
 		memcpy( data.chars, laddr, j );
+
+		/*  print */
 		ptrace( PTRACE_POKEDATA, child, addr + i * 4, NULL );
 	}
 }
@@ -94,7 +116,6 @@ int
 main( int argc, char** argv )
 {
 	pid_t child;
-
 	if( 0 > (child = fork()) ){
 		perror( "fork() failed" );
 	}else if( 0 == child ){
@@ -106,8 +127,8 @@ main( int argc, char** argv )
 		int status;
 		char *str;
 		int toggle = 0;
-		while( 1 ){
 
+		while( 1 ){
 			/* ptrace stopped child, or it exited? */
 			wait( &status );
 			if( WIFEXITED( status ) ){
@@ -122,11 +143,18 @@ main( int argc, char** argv )
 					params[1] = ptrace( PTRACE_PEEKUSER, child, 4 * ECX, NULL );
 					params[2] = ptrace( PTRACE_PEEKUSER, child, 4 * EDX, NULL );
 
-					str = (char*) calloc( (params[2]+1), sizeof(char) );  
+					/* allocation */
+					str = (char*) calloc( (params[2]+1), sizeof(char) );
 
+					/* get data */
 					getdata( child, params[1], str, params[2] );
+
+					/* reverse */
 					reverse( str );
+
+					/* print data */
 					putdata( child, params[1], str, params[2] );
+
 				}else{
 					toggle=0;
 				}
