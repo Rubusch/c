cc6d33495efd88d7f04f9b5c37afa70c32802632
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-03 11:39:15 +0200

    formatting

--- 30__c__pthreads/pthread__deadlock/deadlock.c
+++ 30__c__pthreads/pthread__deadlock/deadlock.c
@@ -1,9 +1,10 @@
 // deadlock.c
 /*
   demonstrates a deadlock using POSIX threads:
-  at pressing enter - after a while and numerous ENTERs the proggy runs into a deadlock,
-  since the mutex is still locked by the counter thread, the main thread put the couter thread into "suspend" 
-  and (because the mutex already is locked), the main thread is not able to lock the mutex for it.
+  at pressing enter - after a while and numerous ENTERs the proggy runs into a
+  deadlock since the mutex is still locked by the counter thread, the main
+  thread put the couter thread into "suspend" and (because the mutex already is
+  locked), the main thread is not able to lock the mutex for it.
 //*/
 
 #include <stdio.h>
@@ -38,7 +39,7 @@ int main()
   pthread_attr_t attr_counter;
   pthread_attr_init(&attr_counter);
   pthread_attr_setdetachstate(&attr_counter, PTHREAD_CREATE_DETACHED);
-  
+
   // create thread (PTHREAD_CREATE_DETACHED)
   pthread_t tid_counter;
   if(0 != pthread_create(&tid_counter, &attr_counter, counter,(void*) 0)){
@@ -62,9 +63,9 @@ int main()
     // unlock
     pthread_mutex_unlock(&lock_count);
 
-    suspend = 0;    
+    suspend = 0;
   }
-  
+
   // destroy thread attribute
   pthread_mutex_destroy(&lock_count);
   pthread_attr_destroy(&attr_counter);
@@ -78,26 +79,27 @@ int main()
 //*/
 void* counter(void* arg)
 {
-  while(1){ 
+  while(1){
     // locking the mutex before "suspend"-if
     pthread_mutex_lock(&lock_count);
 
-    printf("."); 
+    printf(".");
     fflush(stdout);
     int idx;
-    
+
     if(!suspend){ // this happens with a locked mutex -> dangerous, it will end up in a dead lock after a while!
-      ++count;    
-      for (idx = 0; idx < 500; ++idx) // the lower the number, the easier to run into a deadlock here 
+      ++count;
+      for (idx = 0; idx < 500; ++idx) // the lower the number, the easier to run into a deadlock here
 	;
-    
+
       // unlock - only within the "suspend"-if
-      pthread_mutex_unlock(&lock_count); 
-      
-      for (idx = 0; idx < 500; ++idx)
+      pthread_mutex_unlock(&lock_count);
+
+      for (idx = 0; idx < 500; ++idx) {
 	;
+      }
     }
   }
-  pthread_exit((void*) 0);  
+  pthread_exit((void*) 0);
 }
 
