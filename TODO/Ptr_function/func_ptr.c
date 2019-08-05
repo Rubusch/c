c1a8ce33f319a1a4634ac95157130e5a7348c8fc
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-05 19:18:54 +0200

    functionptr: brush up

--- TODO/Ptr_function/func_ptr.c
+++ TODO/Ptr_function/func_ptr.c
@@ -18,22 +18,25 @@ int main(int argc, char** argv)
 {
   printf(" *** function pointer on printf ***\n");
 
-  // function pointer - init
+  // function pointer - definition
   int (*ptr) (const char*, ...);
 
-  // init the function pointer with the func "printf(const char*, ...)"
+  // assign "printf(const char*, ...)" to the ptr
   ptr = printf;
 
-  // use the function pointer, as the function itself
+  // use the function pointer
   (*ptr)("Print out %i and use the function pointer\n", 777);
 
   printf("\n\n *** function pointer array on printf and scanf ***\n\n");
 
-  // func ptr array - init
+  // func ptr array
+
+  // definition and assignment
   int (*ptr_arr[]) (const char*, ...) = {scanf, printf};
   int iVal;
+
   /*
-    for accepting a function to an array of func pointers the RETURN value 
+    for accepting a function to an array of func pointers the RETURN value
     compares to the type of a variable in a common array.
   //*/
 
@@ -44,5 +47,6 @@ int main(int argc, char** argv)
 
   // use the macro
   IO(1)("READY.\n");
-  return EXIT_SUCCESS;
+
+  exit(EXIT_SUCCESS);
 }
