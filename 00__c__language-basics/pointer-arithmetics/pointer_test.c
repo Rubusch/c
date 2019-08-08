c45c7de1e2001c8bac59c78f0d29fea69119452a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-08 13:58:56 +0200

    pointer-arithmetics: fixing bugs, setting Makefile to Werror & pedantic

--- 00__c__language-basics/pointer-arithmetics/pointer_test.c
+++ 00__c__language-basics/pointer-arithmetics/pointer_test.c
@@ -10,54 +10,58 @@
 #include <string.h>
 
 void show_length(char arr[44], char *pArr, char **ppArr){
+  printf("\n\n");
   printf("\n========show_length()===start=================\n\n\n");
 
+  printf("\n\n");
   printf("~~~~~arr[]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-  printf("func()::arr[]\t: '%s\'\n", arr);
+  printf("func()::arr[]:\t\t\"%s\"\n", arr);
   printf("\n");
   printf("sizeof(*arr)/sizeof(char):\t%lu\n", sizeof(*arr)/sizeof(char));
   printf("strlen(arr):\t\t\t%lu plus 1 token \'\\0\'\n", strlen(arr));
   printf("\n");
-  printf("func()::arr:\t\t%s\taddress of the target\n", arr);
-  printf("&func()::arr:\t\t%lu\taddress of the poitner\n", (unsigned long) &arr);
-  printf("*func()::arr:\t\t%i\tcontent of the pointer as int\n", *arr);
-  printf("&*func()::arr:\t\t%s\taddress of the target of the pointer\n", &*arr);
+  printf("func()::arr:\t\t\"%s\"\taddress of the content, interpreted as string by printf\n", arr);
+  printf("&func()::arr:\t\t%lX\t\t\t\t\taddress of the poitner\n", (unsigned long) &arr);
+  printf("*func()::arr:\t\t%i\t\t\t\t\t\tcontent of the pointer 'A' as int 65\n", *arr);
+  printf("&*func()::arr:\t\t\"%s\"\tsame as 'func()::arr'\n", &*arr);
   printf("\n");
-  printf("conclusion:\taddress of target == address of pointer\n"); // conclusion
+  printf("conclusion:\t\taddress of target == address of pointer\n"); // conclusion
 
+  printf("\n\n");
   printf("~~~~~*pArr~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
-  printf("*func()::pArr: '%s\'\n", pArr);
+  printf("*func()::pArr:\t\t\"%s\"\n", pArr);
   printf("\n");
   printf("sizeof(*pArr)/sizeof(char):\t%li\n", sizeof(*pArr)/sizeof(char));   // doesn't work - due to being a *ptr and no arr[]!!!
   printf("strlen(pArr):\t\t\t%li plus 1 token \'\\0\'\n", strlen(pArr));    // only this works with *ptr's!!!
   printf("\n");
-  printf("func()::pArr:\t\t%s\taddress of the target\n", pArr);
-  printf("&func()::pArr:\t\t%lu\taddress of the pointer\n", (unsigned long) &pArr);
-  printf("*func()::pArr:\t\t%i\tcontent of the pointer as int\n", *pArr);
-  printf("&*func()::pArr:\t\t%s\taddress of the target of the pointer\n", &*pArr);
+  printf("func()::pArr:\t\t%s\taddress of the target, interpreted as string by printf\n", pArr);
+  printf("&func()::pArr:\t\t%lX\t\t\t\t\taddress of the pointer\n", (unsigned long) &pArr);
+  printf("*func()::pArr:\t\t%i\t\t\t\t\t\tcontent of the pointer as int\n", *pArr);
+  printf("&*func()::pArr:\t\t\"%s\"\tsame as 'func()::pArr'\n", &*pArr);
   printf("\n");
-  printf("conclusion:\taddress of pointer != address of target\n");
-  printf("\t\tdoesn't work - due to being a *ptr and no arr[]!!!\n");
-  printf("\t\tonly this works with *ptr's!!!\n");
+  printf("conclusion:\t\taddress of pointer != address of target\n");
+  printf("\t\t\tdoesn't work - due to being a *ptr and no arr[]!!!\n");
+  printf("\t\t\tonly this works with *ptr's!!!\n");
 
+  printf("\n\n");
   printf("~~~~~**ppArr~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
-  printf("**func()::ppArr: '%s\'\n", *ppArr);
+  printf("**func()::ppArr:\t\"%s\"\n", *ppArr);
   printf("\n");
   printf("sizeof(**ppArr)/sizeof(char):\t%li\n", sizeof(**ppArr)/sizeof(char)); // doesn't work - due to being a *ptr and no arr[]!!!
   printf("strlen(*ppArr):\t\t\t%li plus 1 token \'\\0\'\n", strlen(*ppArr));  // only this works with *ptr's!!!
   printf("\n");
-  printf("func()::ppArr:\t\t%lu\taddress of the char pointer\n", (unsigned long) ppArr);
-  printf("&func()::ppArr:\t\t%lu\taddress of the pointer to long int\n", (unsigned long) &ppArr);
-  printf("*func()::ppArr:\t\t%s\tcontent of the long int pointer,\n\t\t\taddress of the pointer to char\n", *ppArr);
-  printf("&*func()::ppArr:\t%lu\taddress of the char pointer\n", (unsigned long) &*ppArr);
-  //
-  printf("**func()::ppArr:\t%i\tcontent of the pointer to char, \'A\' as int\n", **ppArr);
-  printf("&**func()::ppArr:\t%s\taddress of the content of the\n\t\t\tpointer to char referenced by the\n\t\t\tpointer to long int\n", &**ppArr);
+  printf("func()::ppArr:\t\t%lX\t\t\t\t\taddress of the char pointer\n", (unsigned long) ppArr);
+  printf("&func()::ppArr:\t\t%lX\t\t\t\t\taddress of the pointer to long int\n", (unsigned long) &ppArr);
+  printf("*func()::ppArr:\t\t\"%s\"\tcontent of the long int pointer, interpreted by printf,\n\t\t\taddress of the pointer to char\n", *ppArr);
+  printf("&*func()::ppArr:\t%lX\t\t\t\t\taddress of the char pointer\n", (unsigned long) &*ppArr);
+  printf("**func()::ppArr:\t%i\t\t\t\t\t\tcontent of the pointer to char, \'A\' as int\n", **ppArr);
+  printf("&**func()::ppArr:\t\"%s\"\tsame as '*func::ppArr'\n", &**ppArr);
   printf("\n");
-  printf("conclusion:\ntype:\t\tchar\t\t\t*char\t\t*long int\n\ncontent:\t**ppArr\t\t\t<-**- *ppArr\t<-*- ppArr\n\naddress:\t&**ppArr, *ppArr\t&*ppArr, ppArr\t&ppArr\n");
+  printf("conclusion:\ntype:\t\tchar\t\t\t*char\t\t*long int\n\ncontent:\t**ppArr\t\t<-**-\t*ppArr\t<-*-\tppArr\n\naddress:\t&**ppArr, *ppArr\t&*ppArr, ppArr\t&ppArr\n");
   //*/
+  printf("\n\n");
   printf("\n========show_length()===end=====================\n\n\n");
 }
 
