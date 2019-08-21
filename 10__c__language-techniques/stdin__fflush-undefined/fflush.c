cdb0e1b51fe961730eff089cc91824e631432124
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-21 17:08:14 +0200

    stdin: example for fflush and stdin added

--- 10__c__language-techniques/stdin__fflush-undefined/fflush.c
+++ 10__c__language-techniques/stdin__fflush-undefined/fflush.c
@@ -1,6 +1,6 @@
 // fflush.c
 /*
-  fflush() can lead to undefined behaviour!!!
+  fflush(stdin) can lead to undefined behaviour!!!
 
 
   #include <stdio.h>
@@ -35,25 +35,7 @@ int main(int argc, char** argv)
     while ((c = getchar()) != EOF && c != '\n');
     memset(str, '\0', sizeof(str));
   }
-  puts("\n");
 
-  puts("stdout: reversed order between buffered and unbuffered stream");
-  char buf[BUFSIZ]; memset(buf, '\0', sizeof(buf));
-  fprintf(stdout, "turn buffer on\n");
-  setvbuf(stdout, buf, _IOFBF, BUFSIZ); // setup buffer for stdout to BUFSIZ
-
-  fprintf(stdout, "1. ipsum lorum primeiro\n");
-  fflush(stdout); // flush - comes at once, then the 'big task'
-
-  fprintf(stdout, "2. ipsum lorum segundo (buffered stdout)\n");
-//  fflush(stdout); // flush - turn this on, for order: 1-2-3
-
-  fprintf(stderr, "3. sleeping (message on stderr)\n");
-  sleep(5); // a 'big task'
-
-
-  // the output order will be: 1-3-2, since 3 is unbuffered and 2 will wait on
-  // the 'big task' (i.e. sleep) to finish
-
+  puts("READY.");
   exit(EXIT_SUCCESS);
 }
