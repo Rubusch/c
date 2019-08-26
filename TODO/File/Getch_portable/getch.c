3fc5ba43da2ee0c3341c0505fc86a2a8a1f5bb98
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-26 16:13:04 +0200

    getch: refactoring

--- TODO/File/Getch_portable/getch.c
+++ TODO/File/Getch_portable/getch.c
@@ -3,29 +3,23 @@
   Doesn't work with gcc on cygwin!
 //*/
 
-#ifndef GETCH_PORTABLE
-#define GETCH_PORTABLE
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#endif
-
-#ifdef __unix__
-#include <termios.h>
-#include <unistd.h>
+#include <termios.h> /* tcsetattr() */
+#include <unistd.h> /* tcsetattr() */
 
 static struct termios new_io;
 static struct termios old_io;
-/*
-  set terminal in cbreak mode
-  set ECHO and ICANON to 0
-  control options: reading brings 1 Byte VMIN=1 VTIME=1
-//*/
+//  set terminal in cbreak mode
+//  set ECHO and ICANON to 0
+//  control options: reading brings 1 Byte VMIN=1 VTIME=1
 
 int cbreak(int fd){
   // save terminal
-  if((tcgetattr(fd, &old_io)) == -1) return -1;
+  if(-1 == (tcgetattr(fd, &old_io))) {
+    return -1;
+  }
   new_io = old_io;
 
   // define cbreak mode
@@ -34,42 +28,38 @@ int cbreak(int fd){
   new_io.c_cc[VTIME] = 0;
 
   // set cbreak mode
-  if((tcsetattr(fd, TCSAFLUSH, &new_io)) == -1)
+  if (-1 == (tcsetattr(fd, TCSAFLUSH, &new_io))) {
     return -1;
+  }
   return 1;
 }
 
 int getch(void)
 {
   int c;
-  if(cbreak(STDIN_FILENO) == -1){
-    printf("cbreak funkction failed\n");
+  if (-1 == cbreak(STDIN_FILENO)) {
+    printf("cbreak function failed\n");
     exit(EXIT_FAILURE);
   }
   c = getchar();
 
-  // reset old terminal mode
+  // reset (very) old terminal mode
   tcsetattr(STDIN_FILENO, TCSANOW, &old_io);
   return c;
 }
 
-#elif __WIN32__ || _MSC_VER || __MS_DOS__
-#include <conio.h>
-#endif
-
 
 /* Demo */
-#define DEMOCODE
-//*/
-#ifdef DEMOCOE
-int main(char** argv, int argc)
+int main(int argc, char** argv)
 {
   int sz;
   printf("Enter \'q\' to terminate the prg.\n");
 
-  while((sz=getch()) != 'q')
-    ;
+  while ((sz=getch()) != 'q') {
+    // do some print out of the read characters
+    puts((char*)&sz);
+  }
 
   return EXIT_SUCCESS;
-};
-#endif
+}
+
