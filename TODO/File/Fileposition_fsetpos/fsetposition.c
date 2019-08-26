e5b91be4ead2f92669f6b9cc975d3f676ba5b7aa
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-26 15:22:25 +0200

    fsetpos: update

--- TODO/File/Fileposition_fsetpos/fsetposition.c
+++ TODO/File/Fileposition_fsetpos/fsetposition.c
@@ -7,6 +7,49 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h> /* memset() */
+
+
+// READ INPUT via fgetc
+void readstring(char* cTxt, const unsigned int txtSize, const char* comment)
+{
+  if(NULL == comment){
+    perror("text is NULL");
+    return;
+  }
+  if(NULL == cTxt){
+    perror("iTxt is NULL");
+    return;
+  }
+
+  do{
+    // reset
+    memset(cTxt, '\0', txtSize);
+    puts(comment);
+
+    // read in
+    unsigned int c;
+    unsigned int idx=0;
+
+    // in case of exceeding the size of the variable - put a '\0' at the end
+    // and read until '\n', but don't store the characters for cleaning the
+    // stream
+    for (idx=0; '\n' != (c = fgetc(stdin)); ++idx) {
+      if ((txtSize-1) > idx) {
+        cTxt[idx] = c;
+      } else if ((txtSize-1) == idx) {
+	puts("input too long - will be reset");
+	memset(cTxt, '\0', txtSize);
+        fflush(stdin);
+	// or cut here:
+	//cTxt[idx] = '\0';
+      } else if (-1 == c) { // EOT, ENQ, ...
+        perror("input was control character");
+        exit(EXIT_FAILURE);
+      }
+    }
+  } while (0 == strlen(cTxt));
+}
 
 int main(int argc, char** argv)
 {
@@ -15,8 +58,12 @@ int main(int argc, char** argv)
   char file[FILENAME_MAX];
   fpos_t pos;
 
+/*
   printf("Open file: ");
   scanf("%s", file);
+/*/
+  readstring(file, FILENAME_MAX, "Open file: ");
+// */
 
   if( (srcStream = fopen(file, "r")) == NULL){
     fprintf(stderr, "Opening %s failed!\n", file);
@@ -25,7 +72,19 @@ int main(int argc, char** argv)
 
   // fgetpos() - show the position
   fgetpos(srcStream, &pos);
-  printf("Position (originally): %li\n\n", pos);
+
+  // fpos_t is not necessarily a scalar (can't be simply typedef'd to something printable)
+  // the hex content is printable, though
+/*
+  printf("Position (originally): %lu\n\n", (unsigned long) pos);
+/*/
+  printf("Position (originally): 0x");
+  for (size_t idx=0; idx < sizeof(pos); ++idx) {
+    printf("%02X", ((unsigned char *) &pos)[idx]);
+  }
+  printf("\n");
+//*/    
+
 
   while((c=getc(srcStream)) != EOF)
     putc(c, stdout);
@@ -34,7 +93,15 @@ int main(int argc, char** argv)
 
   // fsetpos() - reset to the original position
   fsetpos(srcStream, &pos);
-  printf("Position (again): %ld\n\n", pos);
+/*
+  printf("Position (again): %lu\n\n", (unsigned long) pos);
+/*/
+  printf("Position (again): 0x");
+  for (size_t idx=0; idx < sizeof(pos); ++idx) {
+    printf("%02X", ((unsigned char *) &pos)[idx]);
+  }
+  printf("\n");
+//*/    
 
   return EXIT_SUCCESS;
 };
