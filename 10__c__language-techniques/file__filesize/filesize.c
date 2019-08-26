2c9c4abd8dd95ebc442a75fa1b2da20a8c5de104
Lothar Rubusch <l.rubusch@gmx.ch>
2019-08-26 12:12:14 +0200

    filesize: improved input

--- 10__c__language-techniques/file__filesize/filesize.c
+++ 10__c__language-techniques/file__filesize/filesize.c
@@ -9,13 +9,54 @@
 #include <string.h> /* memset() */
 
 
+// READ INPUT via fgetc
+void readstring(char* cTxt, const unsigned int txtSize, const char* comment)
+{
+  if(NULL == comment){
+    perror("text is NULL");
+    return;
+  }
+  if(NULL == cTxt){
+    perror("iTxt is NULL");
+    return;
+  }
+
+  do{
+    // reset
+    memset(cTxt, '\0', txtSize);
+    puts(comment);
+
+    // read in
+    unsigned int c;
+    unsigned int idx=0;
+
+    // in case of exceeding the size of the variable - put a '\0' at the end
+    // and read until '\n', but don't store the characters for cleaning the
+    // stream
+    for (idx=0; '\n' != (c = fgetc(stdin)); ++idx) {
+      if ((txtSize-1) > idx) {
+        cTxt[idx] = c;
+      } else if ((txtSize-1) == idx) {
+	puts("input too long - will be reset");
+	memset(cTxt, '\0', txtSize);
+        fflush(stdin);
+	// or cut here:
+	//cTxt[idx] = '\0';
+      } else if (-1 == c) { // EOT, ENQ, ...
+        perror("input was control character");
+        exit(EXIT_FAILURE);
+      }
+    }
+  } while (0 == strlen(cTxt));
+}
+
 int filesize(char file[FILENAME_MAX], long int* size){
   FILE* srcStream = NULL;
 
   if (!size) return -1;
 
   if( NULL == (srcStream = fopen(file, "r"))){
-    fprintf(stderr, "Opening file %s failed!", file);
+    fprintf(stderr, "Opening file %s failed!\n", file);
     return -1;
   }
 
@@ -31,12 +72,7 @@ int main(int argc, char** argv)
   long int size = 0;
   char file[FILENAME_MAX]; memset(file,'\0', FILENAME_MAX);
 
-  printf("open file: ");
-  if (0 == scanf("%[a-zA-Z.]", file)) { // accept a-z, A-Z and also '.', e.g. enter 'text.txt' should work
-    fprintf(stderr, "scanf failed!\n");
-    return EXIT_FAILURE;
-  }
-
+  readstring(file, FILENAME_MAX, "open file: ");
 
   if (filesize(file, &size)) {
     return EXIT_FAILURE;
