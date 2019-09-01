97f76d6aecc153a11bdca592637881001ecf99cb
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-01 10:56:04 +0200

    file-operations: comments

--- 10__c__language-techniques/file__operations/main.c
+++ 10__c__language-techniques/file__operations/main.c
@@ -20,6 +20,7 @@ int main(int argc, char** argv)
   char text4[] = "He went to bed and bound his head\nWith vinegar and brown paper.\n";
   unsigned long int text1_size = sizeof(text1);
   unsigned long int text2_size = sizeof(text2);
+//  unsigned long int text3_size = sizeof(text3); // unused, only for debugging
   unsigned long int text4_size = sizeof(text4);
   unsigned long int content_size = LIN_LEN;
 
@@ -31,7 +32,7 @@ int main(int argc, char** argv)
   printf("file1 = \'%s\'\nfile2 = \'%s\'\nfile3 = \'%s\'\ntext = %scontent = \'%s\' (empty)\n\n", file1, file2, file3, text1, content);
 
 
-  // write a file characterwise and create write file pointer - OK
+  //* // write a file characterwise and create write file pointer - OK
   puts("1. write a file characterwise and create write file pointer");
   printf("%i - Get write file pointer\n", get_write_file_pointer(&fp, file1));
   printf("%i - Writing file characterwise\n", write_char(fp, text1, text1_size));
@@ -40,19 +41,19 @@ int main(int argc, char** argv)
   printf("Done.\n\n\n");
   //*/
 
-  // creating a new file - OK
+  //* // creating a new file - OK
   puts("2. creating a new file");
   printf("%i - Creating a new file\n", create_file(file2, LIN_LEN));
   printf("Done.\n\n");
   //*/
 
-  // copying unbuffered - OK
+  //* // copying unbuffered - OK
   puts("3. copying unbuffered");
   printf("%i - Copying %s to %s characterwise, unbuffered\n", copy_characterwise_unbuffered(file1, file2), file1, file2);
   printf("Done.\n\n");
   //*/
 
-  // appending a line to file2 - OK
+  //* // appending a line to file2 - OK
   puts("4. appending a line to file2");
   printf("%i - Get append file pointer\n", get_append_file_pointer(&fp, file2));
   printf("%i - Append some text to the file (linewise)\n", write_linewise(fp, text2, text2_size));
@@ -60,7 +61,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // appending a line to file2 - OK
+  //* // appending a line to file2 - OK
   puts("5. appending a line to file2");
   printf("%i - Get append file pointer\n", get_append_file_pointer(&fp, file2));
   printf("%i - Append some text to the file (formated, using printf)\n",  write_formated(fp, text3));
@@ -68,7 +69,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // append a line to file2 (write_char() is already tested!) - OK
+  //* // append a line to file2 (write_char() is already tested!) - OK
   puts("6. append a line to file2 (write_char() is already tested!)");
   printf("%i - Get append file pointer\n", get_append_file_pointer(&fp, file2));
   printf("%i - Append some text to the file (characterwise)\n", write_char(fp, text4, text4_size));
@@ -80,7 +81,7 @@ int main(int argc, char** argv)
   printf("\'%s\' contains now 3 \"lines\" more!\n\n", file2);
   //*/
 
-  // reading linewise and get read file pointer - 3 lines more in file2 - OK
+  //* // reading linewise and get read file pointer - 3 lines more in file2 - OK
   puts("7. reading linewise and get read file pointer - 3 lines more in file2");
   printf("%i - Get read file pointer to %s\n", get_read_file_pointer(&fp, file2), file2);
   printf("%i - Reading from a file linewise\n", read_linewise(fp, &content, &content_size));
@@ -90,7 +91,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // reading file characterwise - OK
+  //* // reading file characterwise - OK
   puts("8. reading file characterwise");
   printf("%i - Get read file pointer to %s\n", get_read_file_pointer(&fp, file2), file2);
   printf("%i - Reading file characterwise\n", read_char(fp, &content, &content_size));
@@ -100,7 +101,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // read blockwise - OK
+  //* // read blockwise - OK
   puts("9. read blockwise");
   printf("%i - Get a Read pointer to %s\n", get_read_file_pointer(&fp, file2), file2);
   printf("%i - Read blockwise\n", read_blockwise(fp, content, content_size));
@@ -110,7 +111,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // reading out without spaces nor line feed - same, 3 lines more - OK
+  //* // reading out without spaces nor line feed - same, 3 lines more - OK
   puts("10. reading out without spaces nor line feed - same, 3 lines more");
   printf("%i - Get read file pointer to %s\n", get_read_file_pointer(&fp, file2), file2);
   printf("%i - Reading without spaces\n", read_without_spaces(fp, &content, &content_size));
@@ -120,27 +121,27 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // copying buffered - without created file2 - OK (already done before)
+  //* // copying buffered - without created file2 - OK (already done before)
   puts("11. copying buffered - without created file2 - OK (already done before)");
   printf("Copy characterwise and buffered...\n");
   printf("%i - Copying %s to %s characterwise, buffered\n", copy_characterwise_buffered(file2, file3, LIN_LEN), file2, file3);
   printf("Done.\n\n");
   //*/
 
-  // copy characterwise and buffered
+  //* // copy characterwise and buffered
   puts("12. copy characterwise and buffered");
   printf("Copy characterwise and unbuffered...\n");
   printf("%i - Copying %s to %s characterwise, unbuffered\n", copy_characterwise_unbuffered(file2, "Jill.txt"), file2, "Jill.txt");
   printf("Done.\n\n");
   //*/
 
-  // renaming the file3 - OK
+  //* // renaming the file3 - OK
   puts("13. renaming the file3");
   printf("%i - Renaming the file \'%s\' to \'Jack.txt\'\n", rename_file(file1, "Jack.txt"), file1);
   printf("Done.\n\n");
   //*/
 
-  // read a single line - OK
+  //* // read a single line - OK
   puts("14. read a single line");
   printf("%i - Check out the read/write pointer to \'%s\'\n", get_read_write_file_pointer(&fp, file3), file3);
   printf("%i - Read the %i. line (starting with 1) of \'%s\'\n", read_nth_line(fp, content, content_size, 3), 3, file3);
@@ -150,7 +151,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // read a line interval - OK
+  //* // read a line interval - OK
   puts("15. read a line interval");
   printf("%i - check out the read/write pointer to \'%s\'\n", get_read_write_file_pointer(&fp, file3), file3);
   printf("%i - Read \'%s\' from line %i until %i\n", read_lines(fp, content, content_size, 2, 4), file3, 2, 4);
@@ -160,8 +161,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  /*
-  // read without eof - OK
+  /* // read without eof - OK
   puts("16. read without eof");
   printf("%i - Check out the read/write pointer to \'%s\'\n", get_read_write_file_pointer(&fp, file3), file3);
   printf("%i - Read \'%s\' without eof\n", read_without_eof(fp, content, content_size), file3);
@@ -180,7 +180,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  //* File Size - OK
+  //* // File Size - OK
   puts("18. File Size");
   unsigned long file_size = 0;
   printf("Check File \"%s\"\n", file3);
@@ -191,7 +191,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // Check EOF - OK
+  //* // Check EOF - OK
   puts("19. Check EOF");
   printf("Check \"%s\" for EOF\n", file3);
   printf("%i - Get a read & write pointer\n", get_read_write_file_pointer(&fp, file3));
@@ -200,7 +200,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // Check for Error - OK
+  //* // Check for Error - OK
   puts("20. Check for Error");
   printf("Check \"%s\" for errors\n", file3);
   printf("%i - Get a read & write pointer\n", get_read_write_file_pointer(&fp, file3));
@@ -210,7 +210,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  //* Shredding file2 - OK
+  //* // Shredding file2 - OK
   puts("21. Shredding file2");
   printf("%i - Get a read & write pointer\n", get_read_write_file_pointer(&fp, file2));
   printf("%i - Shredding \'%s\'\n", shred_file(file2), file2);
@@ -218,7 +218,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  //* Text with pattern - OK
+  //* // Text with pattern - OK
   puts("22. Text with pattern");
   printf("Read text with a pattern\n");
   memset(content, '\0', content_size);
@@ -229,7 +229,7 @@ int main(int argc, char** argv)
   printf("Done.\n\n");
   //*/
 
-  // Temporary files - File: "tmp.txt"
+  //* // Temporary files - File: "tmp.txt"
   puts("23. Temporary files - File: \"tmp.txt\"");
   char szTmp[] = "tmp.txt";
   memset(content, '\0', content_size);
