9570e4d35e70730a2c3b3f64f205725d79df27dd
Lothar Rubusch <l.rubusch@gmx.ch>
2019-09-03 14:23:42 +0200

    pointer: add simple pointer demo, assignment

---
+++ 00__c__language-basics/pointer__assignment/pointer.c
@@ -0,0 +1,19 @@
+// pointer_test.c
+/*
+  demonstrates strlen() and sizeof(a)/sizeof(char)
+  demonstrates the use of pointers and double pointers on chars
+//*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+  // TODO
+
+  return EXIT_SUCCESS;
+}
+
