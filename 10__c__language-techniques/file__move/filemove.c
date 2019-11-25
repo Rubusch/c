6ea3317923153bc27efab1a1f34153f7dafda0d8
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-25 17:40:36 +0100

    filemove: implement two solutions to compare

--- 10__c__language-techniques/file__move/filemove.c
+++ 10__c__language-techniques/file__move/filemove.c
@@ -3,73 +3,72 @@
   moves a file
 //*/
 
+#include <sys/stat.h> /* file_copy() */
+#include <fcntl.h> /* file_copy() */
+#include <unistd.h> /* file_copy() - low level ssize_t, read(), write(),... */
+
+#include <time.h> /* random number generation */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> /* memset() */
 
 
-// READ INPUT via fgetc
-void readstring(char *cTxt, const unsigned int txtSize, const char *comment)
-{
-  if (NULL == comment) {
-    perror("text is NULL");
-    return;
-  }
-  if (NULL == cTxt) {
-    perror("iTxt is NULL");
-    return;
-  }
-
-  do {
-    // reset
-    memset(cTxt, '\0', txtSize);
-    puts(comment);
-
-    // read in
-    unsigned int c;
-    unsigned int idx = 0;
-
-    // in case of exceeding the size of the variable - put a '\0' at the end
-    // and read until '\n', but don't store the characters for cleaning the
-    // stream
-    for (idx = 0; '\n' != (c = fgetc(stdin)); ++idx) {
-      if ((txtSize - 1) > idx) {
-        cTxt[idx] = c;
-      } else if ((txtSize - 1) == idx) {
-        puts("input too long - will be reset");
-        memset(cTxt, '\0', txtSize);
-        fflush(stdin);
-        // or cut here:
-        // cTxt[idx] = '\0';
-      } else if (-1 == c) { // EOT, ENQ, ...
-        perror("input was control character");
-        exit(EXIT_FAILURE);
-      }
-    }
-  } while (0 == strlen(cTxt));
-}
 
 /*
-  NOTE: rename() works for renaming and/or moving files
-  anyway it cannot move cross-device, i.e. it then will
-  throw cross-device link errors (errno).
-
-  This is the case when e.g. moving to a mounted folder
-  e.g. from a docker container to export build artifacts
-
-  The solution then is a hand-made copy function.
- */
-int file_move(char old_filename[FILENAME_MAX], char new_filename[FILENAME_MAX])
+// the classic unix approach
+//
+// NOTE: rename() works for renaming and/or moving files
+// anyway it cannot move cross-device, i.e. it then will
+// throw cross-device link errors (errno).
+//
+// This is the case when e.g. moving to a mounted folder
+// e.g. from a docker container to export build artifacts
+int file_move(const char old_filename[FILENAME_MAX], const char new_filename[FILENAME_MAX])
 {
   return rename(old_filename, new_filename);
 }
 
-/*
-int file_copy(const char old_filename[FILENAME_MAX], char new_filename[FILENAME_MAX])
+/*/
+// the hand made solution
+//
+// this solution has no problems with cross-device links
+// though needs to be tested thoroughly since it is self implemented
+// TODO wtf, avoid THIS?
+#ifndef BUF_SIZE
+#define BUF_SIZE 4096
+#endif
+
+int file_move(const char* src, const char* dst)
 {
-// TODO
-  return 0;
+    int fd_input, fd_output;
+    mode_t perm_file;
+    char buf[BUF_SIZE];
+    ssize_t num;
+
+    fd_input = open(src, O_RDONLY);
+    if (-1 == fd_input)
+        return 1;
+
+    perm_file = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
+    fd_output = open(dst, O_CREAT | O_TRUNC | O_WRONLY, perm_file);
+    if (-1 == fd_output)
+        return 1;
+
+    while (0 < (num = read(fd_input, buf, BUF_SIZE)))
+        if (num != write(fd_output, buf, num))
+            return 1;
+
+    if (-1 == num)
+        return 1;
+    if (-1 == close(fd_input))
+        return 1;
+    if (-1 == close(fd_output))
+        return 1;
+
+    remove(src);
+
+    return 0;
 }
 // */
 
