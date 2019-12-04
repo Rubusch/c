5bb3414eeba7f11826eb9ff4a1281133ecbcb74f
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-04 11:55:57 +0100

    ptrace/syscall: fixed ptrace03

--- 40__c__linux/ptrace03__syscall_getregs-TODO/ptracer.c
+++ 40__c__linux/ptrace03__syscall_getregs-TODO/ptracer.c
@@ -5,10 +5,9 @@
   reads out child's registers %ebx, %ecx and %edx one by one, else it prints
   the %eax register for other syscalls
 
-  reading out registers one by one at syscall can be cumbersome, getregs fetches
-  all registers at a single call
+  reading out registers at syscall WRITE, then printing return value (eax / rax)
 
-FIXME: not working for 64 bit
+  NOTE: for x86_64 it needs to be aligned by 8, for x86 needs to be aligned by 4
 
 
   email: L.Rubusch@gmx.ch
@@ -46,62 +45,51 @@ int main(int argc, char **argv)
   if (0 > (child = fork())) {
     perror("fork() failed");
   } else if (0 == child) {
-    /* child */
+    /* tracee */
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     execl("/bin/pwd", "pwd", NULL);
   } else {
+    /* tracer */
     while (1) {
+      // check if ptrace stopped child, or if it exited
+      wait(&status);
+      if (WIFEXITED(status)) {
+        break;
+      }
+
 #if __x86_64__
-      /* check if ptrace stopped child, or if it exited */
-      wait(&status);
-      if (WIFEXITED(status)) {
-        break;
-      }
-
-      orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_RAX, NULL);
-      if (orig_eax == SYS_write) {
-        if (insyscall == 0) {
-          /* syscall entry */
-          insyscall = 1;
-
-          /* PTRACE: get a handle on all registers */
-          ptrace(PTRACE_GETREGS, child, NULL, &regs);
-          fprintf(stderr, "parent: write called with %llu, %llu, %llu\n",
-                  regs.rbx, regs.rcx, regs.rdx);
-        } else {
-          /* syscall exit */
-          eax = ptrace(PTRACE_PEEKUSER, child, 4 * RAX, NULL);
-          fprintf(stderr, "parent: write returned with %lu\n", eax);
-          insyscall = 0;
-        }
-      }
-      ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+      orig_eax = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
 #else
-      /* check if ptrace stopped child, or if it exited */
-      wait(&status);
-      if (WIFEXITED(status)) {
-        break;
-      }
-
       orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+#endif
       if (orig_eax == SYS_write) {
         if (insyscall == 0) {
-          /* syscall entry */
+          // syscall entry
           insyscall = 1;
 
-          /* get registers */
+          // PTRACE: get a handle on all registers
           ptrace(PTRACE_GETREGS, child, NULL, &regs);
-          fprintf(stderr, "parent: write called with %lu, %lu, %lu\n", regs.ebx,
-                  regs.ecx, regs.edx);
+
+#if __x86_64__ // 64 bit
+          fprintf(stderr, "parent: write called with 0x%08llx [rbx], 0x%08llx [rcx], 0x%08llx [rdx]\n", regs.rbx, regs.rcx, regs.rdx);
+#else          // 32 bit
+          fprintf(stderr, "parent: write called with 0x%04lx [ebx], 0x%04lx [ecx], 0x%04lx [edx]\n", regs.ebx, regs.ecx, regs.edx);
+#endif
+
         } else {
-          /* syscall exit */
+          // syscall exit
+
+#if __x86_64__ // 64 bit
+          eax = ptrace(PTRACE_PEEKUSER, child, 8 * RAX, NULL);
+          fprintf(stderr, "parent: write returned with 0x%08lx [rax]\n", eax);
+#else          // 32 bit
           eax = ptrace(PTRACE_PEEKUSER, child, 4 * EAX, NULL);
-          fprintf(stderr, "parent: write returned with %lu\n", eax);
+          fprintf(stderr, "parent: write returned with 0x%04lx [eax]\n", eax);
+#endif
           insyscall = 0;
         }
       }
       ptrace(PTRACE_SYSCALL, child, NULL, NULL);
-#endif
     }
   }
   return 0;
