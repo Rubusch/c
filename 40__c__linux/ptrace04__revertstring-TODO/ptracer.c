6b5ab6d3eb3cbd0180be5a8be5eb3b548db01eba
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-04 15:12:05 +0100

    ptrace04: work in progress

--- 40__c__linux/ptrace04__revertstring-TODO/ptracer.c
+++ 40__c__linux/ptrace04__revertstring-TODO/ptracer.c
@@ -5,24 +5,20 @@
   PTRACE_PEEKDATA, revert it, and inject the reverted back into the child with
   PTRACE_POKEDATA
 
-FIXME: not working for 64 bit
+  NOTE: the implementation is for x86_64 only!
 
 
-  email: L.Rubusch@gmx.ch
+  author: Lothar Rubusch, L.Rubusch@gmx.ch
 
-  resources: Linux Journal, Nov 30, 2002  By Pradeep Padala ppadala@cise.ufl.edu
-or p_padala@yahoo.com
+  resources
+  Linux Journal, Nov 30, 2002  By Pradeep Padala ppadala@cise.ufl.edu or p_padala@yahoo.com
 */
 
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#if __x86_64__
 #include <sys/reg.h>
-#else
-#include <asm/ptrace-abi.h> /* constants, e.g. ORIG_EAX, etc. */
-#endif
 #include <sys/syscall.h>
 #include <sys/user.h>
 
@@ -30,13 +26,15 @@ or p_padala@yahoo.com
 #include <stdlib.h>
 #include <string.h>
 
-const int long_size = sizeof(long);
+
+const int long_size = sizeof(long long);
+
 
 void get_data(pid_t child, long addr, char *str, int len)
 {
   char *laddr;
   int idx, jdx;
-  /* data structure for reversing the string */
+  // data structure for reversing the string
   union u {
     long val;
     char chars[long_size];
@@ -46,20 +44,20 @@ void get_data(pid_t child, long addr, char *str, int len)
   jdx = len / long_size;
   laddr = str;
   while (idx < jdx) {
-    /* get 'val' for data */
+    // get 'val' for data
     data.val = ptrace(PTRACE_PEEKDATA, child, addr + idx * 8, NULL);
 
-    /* copy over to 'chars' */
+    // copy over to 'chars'
     memcpy(laddr, data.chars, long_size);
 
-    /* increment read index */
+    // increment read index
     ++idx;
 
-    /* increment write position - move pointer */
+    // increment write position - move pointer
     laddr += long_size;
   }
 
-  /* copy over remaining characters to 'chars' */
+  // copy over remaining characters to 'chars'
   jdx = len % long_size;
   if (jdx != 0) {
     data.val = ptrace(PTRACE_PEEKDATA, child, addr + idx * 8, NULL);
@@ -71,14 +69,29 @@ void get_data(pid_t child, long addr, char *str, int len)
 
 void reverse(char *str)
 {
+/*
   int idx, jdx;
   char temp;
-  for (idx = 0, jdx = strlen(str) - 2; idx <= jdx; ++idx, --jdx) {
-    /* swap */
+  for (idx = 0, jdx = strlen(str)-2; idx <= jdx; ++idx, --jdx) { // FIXME
     temp = str[idx];
     str[idx] = str[jdx];
     str[jdx] = temp;
   }
+/*/
+  int len;
+  int idx, jdx;
+  char temp;
+  for (idx = 0, jdx = strlen(str)-2; idx <= jdx; ++idx, --jdx) { // FIXME: why '2' and not '1'?
+    temp = str[idx];
+    fprintf(stderr, "%s[%d]:     temp '%c'\n", __func__, __LINE__, temp);
+    str[idx] = str[jdx];
+    str[jdx] = temp;
+  }
+
+  len = strlen(str);
+  fprintf(stderr, "%s[%d]:     str '%s', len = %d\n", __func__, __LINE__, str, len);
+// */
+
 }
 
 
@@ -95,26 +108,26 @@ void put_data(pid_t child, long addr, char *str, int len)
   jdx = len / long_size;
   laddr = str;
   while (idx < jdx) {
-    /* copy 'chars' */
+    // copy 'chars'
     memcpy(data.chars, laddr, long_size);
 
-    /* write from 'val' */
+    // write from 'val'
     ptrace(PTRACE_POKEDATA, child, addr + idx * 8, data.val);
 
-    /* next to read */
+    // next to read
     ++idx;
 
-    /* next to write */
+    // next to write
     laddr += long_size;
   }
 
-  /* remaining characters... */
+  // remaining characters...
   jdx = len % long_size;
   if (jdx != 0) {
-    /* copy value to 'chars' */
+    // copy value to 'chars'
     memcpy(data.chars, laddr, jdx);
 
-    /*  print */
+    //  print
     ptrace(PTRACE_POKEDATA, child, addr + idx * 8, NULL);
   }
 }
@@ -132,19 +145,19 @@ int main(int argc, char **argv)
     perror("fork() failed");
 
   } else if (0 == child) {
-    /* declare traceme, for being able to read out data of the child process */
+    /* tracee */
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
     execl("/bin/pwd", "pwd", NULL);
 
   } else {
+    /* tracer */
     long orig_eax;
     long params[3];
     int status;
     char *str;
     int toggle = 0;
-
     while (1) {
-      /* ptrace stopped child, or it exited? */
+      // ptrace stopped child, or it exited?
       wait(&status);
       if (WIFEXITED(status)) {
         break;
@@ -153,36 +166,49 @@ int main(int argc, char **argv)
       orig_eax = ptrace(PTRACE_PEEKUSER, child, 8 * ORIG_RAX, NULL);
       if (SYS_write == orig_eax) {
         if (0 == toggle) {
-
-          /* turn peek-reverse-poke off */
+          // turn peek-reverse-poke off
           toggle = 1;
 
+          /* params[0] = ptrace(PTRACE_PEEKUSER, child, 8 * RBX, NULL); */
+          /* params[1] = ptrace(PTRACE_PEEKUSER, child, 8 * RCX, NULL); */
+          /* params[2] = ptrace(PTRACE_PEEKUSER, child, 8 * RDX, NULL); */
+
+// FIXME: are the registers correct for x86_64? as arguments
           params[0] = ptrace(PTRACE_PEEKUSER, child, 8 * RBX, NULL);
           params[1] = ptrace(PTRACE_PEEKUSER, child, 8 * RCX, NULL);
           params[2] = ptrace(PTRACE_PEEKUSER, child, 8 * RDX, NULL);
 
-          /* allocation */
+          // allocation
           str = ( char * )calloc((params[2] + 1), sizeof(char));
+          fprintf(stderr, "AAA str = '%s'\n", str);
 
-          /* get data */
+          // get data
           get_data(child, params[1], str, params[2]);
+          fprintf(stderr, "BBB str = '%s'\n", str);
 
-          /* reverse */
+          // reverse
           reverse(str);
+          fprintf(stderr, "CCC str = '%s'\n", str);
 
-          /* print data */
+          // print data
           put_data(child, params[1], str, params[2]);
 
+          free(str);
+          str = NULL;
+
         } else {
-          /* turn on peek-reverse-poke of this if-cluase */
+          // turn on peek-reverse-poke of this if-cluase
           toggle = 0;
 
-          /* append linefeed */
+          // append linefeed
           puts("");
         }
       }
+fprintf(stderr, "111\n");   // XXX 
       ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+fprintf(stderr, "222\n");   // XXX 
     }
+fprintf(stderr, "333\n");   // XXX 
   }
   exit(EXIT_SUCCESS);
 }
