85055b16120361587f95144588b4711c9ce31670
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-06 16:57:53 +0100

    ptrace/06: update

--- 40__c__linux/ptrace__06-attach-detatch/ptracer.c
+++ 40__c__linux/ptrace__06-attach-detatch/ptracer.c
@@ -6,12 +6,6 @@
   PTRACE_DETACH
 
   usage: compile it (Makefile) and in one shell window run
-  $ ./rabbit.exe
-
-  in another shell window run, within 10 sec
-  $ ./ptracer.exe $(pidof rabbit.exe)
-
-  or
   $ ./rabbit.exe & ./ptracer.exe $(pidof rabbit.exe)
 
   ptrace.exe prints the next instruction
@@ -23,6 +17,11 @@
   or p_padala@yahoo.com
 */
 
+// kill()
+#define _POSIX_SOURCE
+#include <signal.h>
+
+// ptrace(), kill,..
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/user.h>
@@ -38,32 +37,37 @@ int main(int argc, char **argv)
   pid_t traced_process;
   struct user_regs_struct regs;
   long ins;
+
+#ifndef __x86_64__
+  fprintf(stderr, "Source needs x86_64 to run!!\n");
+  exit(EXIT_FAILURE);
+#endif
+
   if (argc != 2) {
     printf("Usage: %s <pid to be traced: %s>\n", argv[0], argv[1]);
-    printf("e.g.:\nopen another shell and start the rabbit.exe, then change "
-           "back to this shell and execute:\n$ %s $(pidof ./rabbit.exe)\n",
-           argv[0]);
+    printf("e.g.:\n$ ./rabbit.exe & %s $(pidof rabbit.exe)\n", argv[0]);
     exit(EXIT_FAILURE);
   }
   traced_process = atoi(argv[1]);
 
-  /* attach to the child process */
+  // attach to the child process
   ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
 
   wait(NULL);
 
-  /* read out registers */
+  // read out registers
   ptrace(PTRACE_GETREGS, traced_process, NULL, &regs);
-#if __x86_64__
-  /* read out instruction */
+
+  // read out instruction
   ins = ptrace(PTRACE_PEEKTEXT, traced_process, regs.rip, NULL);
-  printf("RIP: %llx instruction executed: '%lx'\n", regs.rip, ins);
-#else
-  /* read out instruction */
-  ins = ptrace(PTRACE_PEEKTEXT, traced_process, regs.eip, NULL);
-  printf("EIP: %lx instruction executed: '%lx'\n", regs.eip, ins);
-#endif
-  /* detach */
+  fprintf(stderr, "RIP: process %d with instruction pointer '0x%08lx' [rip] executed instruction: '0x%08lx' [ins]\n", traced_process, (long)regs.rip, (long)ins);
+
+  // detach
   ptrace(PTRACE_DETACH, traced_process, NULL, NULL);
+
+  // clean up
+  fprintf(stderr, "...killing the lab animal...\n");
+  kill(traced_process, SIGSTOP);
+
   return 0;
 }
