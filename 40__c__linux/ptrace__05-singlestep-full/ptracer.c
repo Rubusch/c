bcad30eabf222fdbdbb5b38a01447c6c806792e0
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-06 13:44:54 +0100

    ptrace/05: full example, extend with syscall evaluation

--- 40__c__linux/ptrace__05-singlestep-full/ptracer.c
+++ 40__c__linux/ptrace__05-singlestep-full/ptracer.c
@@ -44,6 +44,7 @@ int main(int argc, char **argv)
     struct user_regs_struct regs;
     long count = 0;
     long ins;
+
     while (1) {
 #if __x86_64__
       // child still alive
@@ -51,13 +52,40 @@ int main(int argc, char **argv)
       if (WIFEXITED(status))
         break;
 
+      // display syscall
+      switch(regs.orig_rax) {
+#ifdef SYS_read
+      case SYS_read:
+        fprintf(stderr, "\tSYSCALL: SYS_read, '0x%04lx'\n", (long)regs.orig_rax);
+        break;
+#endif
+#ifdef SYS_write
+      case SYS_write:
+        fprintf(stderr, "\tSYSCALL: SYS_write, '0x%04lx'\n", (long)regs.orig_rax);
+        break;
+#endif
+#ifdef SYS_open
+      case SYS_execve:
+        fprintf(stderr, "\tSYSCALL: SYS_execve, '0x%04lx'\n", (long)regs.orig_rax);
+        break;
+#endif
+#ifdef SYS_exit_group
+      case SYS_exit_group:
+        fprintf(stderr, "\tSYSCALL: SYS_exit_group, '0x%04lx'\n", (long)regs.orig_rax);
+        break;
+#endif
+
+      default:
+        fprintf(stderr, "\tSYSCALL: uncaught or no syscall\n");
+      }
+
       // read out registers -> regs for instruction pointer
       ptrace(PTRACE_GETREGS, child, NULL, &regs);
 
       // get ins by regs.eip
       ins = ptrace(PTRACE_PEEKTEXT, child, regs.rip, NULL);
-      printf("%ld. RIP: %llx Instruction executed: %lx\n", count, regs.rip,
-             ins);
+      printf("%ld. RIP: 0x%08llx Instruction executed: 0x%016lx\n"
+             , count, regs.rip, ins);
 
       // turn on PTRACE_SINGLESTEP
       ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);
@@ -75,7 +103,8 @@ int main(int argc, char **argv)
 
       // get ins by regs.eip
       ins = ptrace(PTRACE_PEEKTEXT, child, regs.eip, NULL);
-      printf("%ld. EIP: %lx Instruction executed: %lx\n", count, regs.eip, ins);
+      printf("%ld. EIP: 0x%08lx Instruction executed: 0x%08lx\n"
+             , count, regs.eip, ins);
 
       // turn on PTRACE_SINGLESTEP
       ptrace(PTRACE_SINGLESTEP, child, NULL, NULL);
