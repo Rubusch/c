969f7d17f7e09f0d894474e300764e0be20a7589
Lothar Rubusch <l.rubusch@gmx.ch>
2019-12-18 13:22:13 +0100

    ptrace/070: update

--- 40__c__linux/ptrace__070-injection-in-free-space__TODO/ptracer.c
+++ 40__c__linux/ptrace__070-injection-in-free-space__TODO/ptracer.c
@@ -1,37 +1,47 @@
 /*
   inject "hello world" in free space
 
-  attach an external process by declaring PTRACE_ATTACH;
-
-  request its instruction (PTRACE_PEEKDATA) from process's registers
-  (PTRACE_GETREGS), backup instructions and inject (PTRACE_POKEDATA) a "hello
-  world" sequence as hex into some free area, in order to not break anything;
-
-  write to instruction (PTRACE_POKEDATA) and set registers (PTRACE_SETREGS)
-
-  continue the process (PTRACE_CONT) and let it run into the trap
-
-  in the tracer wait, then replace the injected "hello world" in the process by
-  the former, backuped, code (PTRACE_POKEDATA), and inject it into process's
-  registers (PTRACE_SETREGS)
-
-  when done, detaches with PTRACE_DETACH
 
 
-  usage: compile it (Makefile) and in one shell window run
-  $ ./rabbit.exe
+  attach to a running rabbit.exe process with PTRACE_SEIZE and
+  PTRACE_INTERRUPT
 
-  in another shell window run, within 10 sec
-  $ ./ptracer.exe `pidof rabbit.exe`
+  in the tracer obtain the current regs and instruction with PTRACE_GETREGS
+
+  backup instruction pointer, rip
 
   it is best to store the instructions in free space, when injecting, so this
   version searches for "free space" to inject the instructions to
 
+  replace the current instruction with some assembled (via gdb) sequence of
+  another program which ends with a breakpoint SIGTRAP signal,
 
-  email: L.Rubusch@gmx.ch
+  continue the tracee rabbit.exe
 
-  resources: Linux Journal, Nov 30, 2002  By Pradeep Padala ppadala@cise.ufl.edu
-  or p_padala@yahoo.com
+  detach the rabbit.exe with PTRACE_DETACH
+
+  rabbit.exe will now print "Hello XXX World" and end in a SIGTRAP
+
+  stop at the breakpoint
+
+
+  ---
+
+  NOTE: error code handling of ptrace() calls are removed to make it more
+  readable, for productive code consider error code evaluation
+
+  if (0 > ptrace(...)) {
+      // free resources
+      perror("something failed");
+      exit(EXIT_FAILURE);
+  }
+
+
+  AUTHOR: Lothar Rubusch, L.Rubusch@gmx.ch
+
+
+  RESOURCES:
+  * Linux Journal, Nov 30, 2002  By Pradeep Padala ppadala@cise.ufl.edu or p_padala@yahoo.com
 */
 
 #include <sys/ptrace.h>
@@ -46,13 +56,13 @@
 
 const int long_size = sizeof(long);
 
-void get_data(pid_t child, long addr, char *str, int len)
+void get_data(pid_t child, long addr, unsigned char *str, int len)
 {
-  char *laddr;
+  unsigned char *laddr;
   int i, j;
   union u {
     long val;
-    char chars[long_size];
+    unsigned char chars[long_size];
   } data;
   i = 0;
   j = len / long_size;
@@ -72,13 +82,13 @@ void get_data(pid_t child, long addr, char *str, int len)
 }
 
 
-void put_data(pid_t child, long addr, char *str, int len)
+void put_data(pid_t child, long addr, unsigned char *str, int len)
 {
-  char *laddr;
+  unsigned char *laddr;
   int i = 0, j = len / long_size;
   union u {
     long val;
-    char chars[long_size];
+    unsigned char chars[long_size];
   } data;
   laddr = str;
   while (i < j) {
@@ -104,7 +114,7 @@ long freespaceaddr(pid_t pid)
   char filename[30];
   char line[85];
   long addr;
-  char str[20];
+  unsigned char str[20];
 
   sprintf(filename, "/proc/%d/maps", pid);
   fp = fopen(filename, "r");
@@ -112,8 +122,8 @@ long freespaceaddr(pid_t pid)
     exit(EXIT_FAILURE);
   }
   while (fgets(line, 85, fp) != NULL) {
-    sscanf(line, "%lx-%hhx %s", &addr, str, str);
-    if (strcmp(str, "00:00") == 0) {
+    sscanf(line, "%lx-%hhx %s", (long unsigned int*) &addr, str, str);
+    if (strcmp((char*)str, "00:00") == 0) {
       break;
     }
   }
@@ -127,12 +137,12 @@ int main(int argc, char *argv[])
   pid_t traced_process;
   struct user_regs_struct oldregs, regs;
   int len = 41;
-  char insertcode[] = "\xeb\x15\x5e\xb8\x04\x00"
+  unsigned char insertcode[] = "\xeb\x15\x5e\xb8\x04\x00"
                       "\x00\x00\xbb\x02\x00\x00\x00\x89\xf1\xba"
                       "\x0c\x00\x00\x00\xcd\x80\xcc\xe8\xe6\xff"
                       "\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f"
                       "\x72\x6c\x64\x0a\x00";
-  char backup[len];
+  unsigned char backup[len];
   long addr;
 
   if (argc != 2) {
@@ -149,44 +159,44 @@ int main(int argc, char *argv[])
 
   traced_process = atoi(argv[1]);
 
-  /* attach external process */
-  ptrace(PTRACE_ATTACH, traced_process, NULL, NULL);
-
+  // attach external process
+  ptrace(PTRACE_SEIZE, traced_process, NULL, NULL);
+  ptrace(PTRACE_INTERRUPT, traced_process, NULL, NULL);
   wait(NULL);
 
-  /* get process's registers */
+  // get process's registers
   ptrace(PTRACE_GETREGS, traced_process, NULL, &regs);
 
-  /* find some free space for injection */
+  // find some free space for injection
   addr = freespaceaddr(traced_process);
 
-  /* get current instructions */
+  // get current instructions
   get_data(traced_process, addr, backup, len);
 
-  /* inject new instructions */
+  // inject new instructions
   put_data(traced_process, addr, insertcode, len);
 
   memcpy(&oldregs, &regs, sizeof(regs));
   regs.rip = addr;
 
-  /* write registers back */
+  // write registers back
   ptrace(PTRACE_SETREGS, traced_process, NULL, &regs);
 
-  /* let external process continue */
+  // let external process continue
   ptrace(PTRACE_CONT, traced_process, NULL, NULL);
 
   wait(NULL);
   printf("The process stopped, Putting back the original instructions\n");
 
-  /* restore backuped instructions */
+  // restore backuped instructions
   put_data(traced_process, addr, backup, len);
 
-  /* set registers */
+  // set registers
   ptrace(PTRACE_SETREGS, traced_process, NULL, &oldregs);
 
   printf("Letting it continue with original flow\n");
 
-  /* detach process */
+  // detach process
   ptrace(PTRACE_DETACH, traced_process, NULL, NULL);
 
   return 0;
