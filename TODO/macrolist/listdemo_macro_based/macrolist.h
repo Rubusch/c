4bafcdc0d8d77d30c43e73a2c0969568587925c5
Lothar Rubusch <l.rubusch@gmx.ch>
2020-05-15 13:25:12 +0200

    macrolist: started macro based lists

---
+++ TODO/macrolist/listdemo_macro_based/macrolist.h
@@ -0,0 +1,97 @@
+// dl_list.h
+/*
+  a simple single-linked queue example
+//*/
+
+#ifndef DOUBLE_LINKED_LIST
+#define DOUBLE_LINKED_LIST
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DATASIZ 64
+
+typedef struct element {
+  struct element *next;
+  char data[DATASIZ];
+} elements_t;
+
+static elements_t *first = NULL;
+static elements_t *last = NULL;
+
+/* append a new element containing data */
+static int elements__append(const char data[DATASIZ])
+{
+	elements_t *elem;
+
+	if (NULL == (elem = malloc(sizeof(*elem))))
+		return -1;
+
+	elem->next = NULL;
+	strcpy(elem->data, data);
+
+	if (!first) {
+		first = elem;
+	} else {
+		last->next = elem;
+	}
+	last = elem;
+
+	return 0;
+}
+
+/* remove one element containing data */
+static int elements__remove(const char data[DATASIZ])
+{
+	elements_t *elem = first;
+	elements_t *elem_before = NULL;
+
+	if (!first)
+		return -1;
+
+	while (elem) {
+		if (0 == strcmp(data, elem->data)) {
+			if (elem_before)
+				elem_before->next = elem->next;
+			free(elem);
+			return 0;
+		}
+		elem_before = elem;
+		elem = elem->next;
+	}
+
+	return -1;
+}
+
+/* remove all elements */
+static int elements__removeall()
+{
+	elements_t *elem = first;
+
+	while (first) {
+		elem = first->next;
+		free(first);
+		first = elem;
+	}
+
+	return 0;
+}
+
+/* print all elements' data */
+static void elements__print()
+{
+	elements_t* elem = first;
+
+	if (!first) {
+		puts ("empty");
+		return;
+	}
+
+	while (elem) {
+		printf("%s(): '%s'\n", __func__, elem->data);
+		elem = elem->next;
+	}
+}
+
+#endif
---
+++ TODO/macrolist/listdemo_macro_based/main.c
@@ -0,0 +1,49 @@
+// main.c
+/*
+  main file to start the proggy
+//*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "macrolist.h"
+
+
+int main()
+{
+	puts("uninitialized");
+	elements__print();
+	printf("expected: [empty]\n\n");
+
+	puts("init 1");
+	elements__append("AAA");
+	elements__print();
+	printf("expected: [AAA]\n\n");
+
+	puts("append another element");
+	elements__append("BBB");
+	elements__print();
+	printf("expected: [AAA, BBB]\n\n");
+
+	puts("append another element");
+	elements__append("CCC");
+	elements__print();
+	printf("expected: [AAA, BBB, CCC]\n\n");
+
+	puts("remove element");
+	elements__remove("BBB");
+	elements__print();
+	printf("expected: [AAA, CCC]\n\n");
+
+	puts("remove non-existend element");
+	elements__remove("BBB");
+	elements__print();
+	printf("expected: [AAA, CCC]\n\n");
+
+	puts("remove all elements");
+	elements__removeall();
+	elements__print();
+	printf("expected: [empty]\n\n");
+
+	exit(EXIT_FAILURE);
+}
