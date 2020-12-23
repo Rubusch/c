e476863a947766d294f2c4bebaa4cfc4481e6163
Lothar Rubusch <l.rubusch@gmx.ch>
2020-12-23 17:48:58 +0100

    sockets: tcp server 1 - synchronize and refac

--- 40__c__sockets/tcp-server-types__1-tcp-concurrent-server-one-child-per-client/server.c
+++ 40__c__sockets/tcp-server-types__1-tcp-concurrent-server-one-child-per-client/server.c
@@ -68,15 +68,9 @@ static void err_doit(int errnoflag, const char *fmt, va_list ap)
 	char buf[MAXLINE + 1]; memset(buf, '\0', sizeof(buf));
 
 	errno_save = errno; // value caller might want printed
-
-#ifdef HAVE_VSNPRINTF
 	vsnprintf(buf, MAXLINE, fmt, ap); // safe
-#else
-	vsprintf(buf, fmt, ap); // not safe
-#endif
-
 	n_len = strlen(buf);
-	if(errnoflag){
+	if (errnoflag) {
 		snprintf(buf + n_len, MAXLINE - n_len, ": %s", strerror(errno_save));
 	}
 
@@ -217,6 +211,14 @@ pid_t lothars__fork(void)
 }
 
 
+void lothars__setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
+{
+	if (0 > setsockopt(fd, level, optname, optval, optlen)) {
+		err_sys("setsockopt error");
+	}
+}
+
+
 void lothars__listen(int fd, int backlog)
 {
 	if (0 > listen(fd, backlog)) {
@@ -280,14 +282,6 @@ int lothars__tcp_listen(const char *host, const char *serv, socklen_t *addrlenp)
 }
 
 
-void lothars__setsockopt(int fd, int level, int optname, const void *optval, socklen_t optlen)
-{
-	if (0 > setsockopt(fd, level, optname, optval, optlen)) {
-		err_sys("setsockopt error");
-	}
-}
-
-
 void lothars__write(int fd, void *ptr, size_t nbytes)
 {
 	if (nbytes != write(fd, ptr, nbytes)) {
@@ -308,9 +302,11 @@ void lothars__close(int fd)
 // child implementation
 
 /*
-  child routine: do anything, read, write, etc.. some action
+  worker - routine
+
+  do anything, read, write, etc.. some action
 */
-void child_routine(int fd_sock)
+void worker_routine(int32_t fd_sock)
 {
 	int32_t towrite;
 	ssize_t n_read;
@@ -390,21 +386,22 @@ void pr_cpu_time()
 	sys = (double) usage_parent.ru_stime.tv_sec + usage_parent.ru_stime.tv_usec / 1000000.0;
 	sys += (double) usage_child.ru_stime.tv_sec + usage_child.ru_stime.tv_usec / 1000000.0;
 
-	// print message at termination of server
-	printf("\nuser time = %gs, sys time = %gs\n", user, sys);
+	fprintf(stdout, "\nuser time = %gs, sys time = %gs\n", user, sys);
 }
 
 
 /*
   int sig handler - action when server is shutdown by CTRL + c
 */
-void sig_int(int signo)
+void sig_int(int32_t signo)
 {
 	pr_cpu_time();
 	exit(EXIT_SUCCESS);
 }
 
+
 /********************************************************************************************/
+
 
 /*
   main
@@ -456,7 +453,7 @@ int main(int argc, char** argv)
 			lothars__close(fd_listen);
 
 			// process loop - never returnes
-			child_routine(fd_conn);
+			worker_routine(fd_conn);
 
 			exit(EXIT_SUCCESS);
 		}
