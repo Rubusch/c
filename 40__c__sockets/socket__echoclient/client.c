728c25adeddd5a88c3eebbee995ae8f4f375b45b
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-05 21:50:03 +0100

    sockets: tcp echo client - working

--- 40__c__sockets/socket__echoclient/client.c
+++ 40__c__sockets/socket__echoclient/client.c
@@ -1,28 +1,32 @@
-// echoclient.c
 /*
+ * tcp echo client
+ *
+ * sends a prepare message as string to a tcp echo server, the
+ * received string will have maintained escape sequences
+ *
  * @author: Lothar Rubusch
  * @email: L.Rubusch@gmx.ch
  * @license: GPLv3
  * @date: 2013-april-28
  */
 
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <stdarg.h> /* va_start(), va_end(),... */
 #include <unistd.h>
+#include <errno.h>
 
 /*
   constants
 */
 
-#define SERV_PORT 12345
-#define MAXSIZE 4096
+#define MAXLINE  4096 /* max text line length */
 
 
 /*
@@ -34,8 +38,12 @@ void err_sys(const char *, ...);
 
 // sock
 void lothars__connect(int, const struct sockaddr *, socklen_t);
+ssize_t lothars__recv(int, void *, size_t, int);
 void lothars__send(int, const void *, size_t, int);
 int lothars__socket(int, int, int);
+
+// unix
+void lothars__close(int);
 
 
 /*
@@ -63,8 +71,6 @@ static void err_doit(int errnoflag, const char *fmt, va_list ap)
 	fflush(stdout);  // in case stdout and stderr are the same
 	fputs(buf, stderr);
 	fflush(stderr);
-
-	return;
 }
 
 
@@ -87,13 +93,157 @@ void err_sys(const char *fmt, ...)
 }
 
 
-       
+/*
+  The connect() function shall attempt to make a connection on a socket.
+
+  @fd: Specifies the file descriptor associated with the socket.
+
+  @sa: Points to a sockaddr structure containing the peer address. The
+      length and format of the address depend on the address family of
+      the socket.
+
+  @salen: Specifies the length of the sockaddr structure pointed to by
+      the address argument.
+*/
+void lothars__connect(int fd, const struct sockaddr *sa, socklen_t salen)
+{
+	if (0 > connect(fd, sa, salen)) {
+		err_sys("connect error");
+	}
+}
+
+
+/*
+  The recv() function shall receive data from a connection-mode or
+  connectionless-mode socket. It is normally used with connected
+  sockets because it does not permit the application to retrieve the
+  source address of received data.
+
+  @fd: Specifies the socket file descriptor.
+
+  @ptr: Points to a buffer where the message should be stored.
+
+  @nbytes: Specifies the length in bytes of the buffer pointed to by
+      the buffer argument.
+
+  @flags: Specifies the type of message reception. Values of this
+      argument are formed by logically OR'ing zero or more of the
+      following values:
+
+      MSG_PEEK
+        Peeks at an incoming message. The data is treated as unread
+        and the next recv() or similar function shall still return
+        this data.
+
+      MSG_OOB
+        Requests out-of-band data. The significance and semantics of
+        out-of-band data are protocol-specific.
+
+      MSG_WAITALL
+        On SOCK_STREAM sockets this requests that the function block
+        until the full amount of data can be returned. The function
+        may return the smaller amount of data if the socket is a
+        message-based socket, if a signal is caught, if the connection
+        is terminated, if MSG_PEEK was specified, or if an error is
+        pending for the socket.
+*/
+ssize_t lothars__recv(int fd, void *ptr, size_t nbytes, int flags)
+{
+	ssize_t res;
+	if (0 > (res = recv(fd, ptr, nbytes, flags))) {
+		err_sys("recv error");
+	}
+	return res;
+}
+
+
+/*
+  The send() function shall initiate transmission of data from the
+  specified socket to its peer. The send() function shall send a
+  message only when the socket is connected (including when the peer
+  of a connectionless socket has been set via connect()).
+
+  @fd: Specifies the socket file descriptor.
+
+  @ptr: Points to the buffer containing the message to send.
+
+  @nbytes: Specifies the length of the message in bytes.
+
+  @flags: Specifies the type of message transmission. Values of this
+      argument are formed by logically OR'ing zero or more of the
+      following flags:
+
+        MSG_EOR
+          Terminates a record (if supported by the protocol).
+
+	MSG_OOB
+          Sends out-of-band data on sockets that support out-of-band
+          communications. The significance and semantics of
+          out-of-band data are protocol-specific.
+*/
+void lothars__send( int fd
+	    , const void *ptr
+	    , size_t nbytes
+	    , int flags)
+{
+	if (send(fd, ptr, nbytes, flags) != (ssize_t) nbytes) {
+		err_sys("send error");
+	}
+}
+
+
+/*
+  The socket() function shall create an unbound socket in a
+  communications domain, and return a file descriptor that can be used
+  in later function calls that operate on sockets.
+
+  @family: Specifies the communications domain in which a socket is to
+      be created.
+
+  @type: Specifies the type of socket to be created.
+
+  @protocol: Specifies a particular protocol to be used with the
+      socket. Specifying a protocol of 0 causes socket() to use an
+      unspecified default protocol appropriate for the requested
+      socket type.
+*/
+int lothars__socket(int family, int type, int protocol)
+{
+	int res;
+	if (0 > (res = socket(family, type, protocol))) {
+		err_sys("socket error");
+	}
+	return res;
+}
+
+
+/*
+  The close() function shall deallocate the file descriptor indicated
+  by fd. To deallocate means to make the file descriptor available for
+  return by subsequent calls to open() or other functions that
+  allocate file descriptors. All outstanding record locks owned by the
+  process on the file associated with the file descriptor shall be
+  removed (that is, unlocked).
+
+  If close() is interrupted by a signal that is to be caught, it shall
+  return -1 with errno set to [EINTR] and the state of fildes is
+  unspecified. If an I/O error occurred while reading from or writing
+  to the file system during close(), it may return -1 with errno set
+  to [EIO]; if this error is returned, the state of fildes is
+  unspecified.
+
+  @fd: The file descriptor to the specific connection.
+*/
+void lothars__close(int fd)
+{
+	if (-1 == close(fd)) {
+		err_sys("close error");
+	}
+}
 
 
 /********************************************************************************************/
 // worker implementation
-
-
 
 
 /********************************************************************************************/
@@ -106,11 +256,11 @@ int main(int argc, char *argv[])
 {
 	int fd_sock;
 	struct sockaddr_in serveraddr;
-	char buf[MAXSIZE]; memset(buf, '\0', MAXSIZE);
+	char buf[MAXLINE]; memset(buf, '\0', MAXLINE);
 	unsigned int echolen;
 	int nbytes = 0;
 	int read = 0;
-	char text[4096]; memset(text, '\0', 4096);
+	char message[4096]; memset(message, '\0', 4096);
 	char serverip[16]; memset(serverip, '\0', sizeof(serverip));
 	char port[16]; memset(port, '\0', sizeof(port));
 
@@ -124,54 +274,44 @@ int main(int argc, char *argv[])
 	strncpy(port, argv[2], sizeof(port));
 	fprintf(stdout, "port: '%s'\n", port);
 
-	strcpy(text,
+	/*
+	  message
+
+	  a lot of escapes, will be sent and received as string, thus
+	  escape sequences will persist in the received message
+	 */
+	strcpy(message,
 	       "<?xml version=\"1.0\"?> \n<TASK task_name=\"guiagent\">\n  <DATA_OUT "
 	       "name=\"xxx\" dim=\"0\" type=\"cmd\" value=\"agtstart\">\n <DST "
 	       "name=\"agentname\" />\n </DATA_OUT>\n </TASK>");
 
 	fprintf(stdout, "socket()\n");
 	fd_sock = lothars__socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-//	if (0 > (fd_sock = socket(PF_INET, SOCK_STREAM,  
-//				 IPPROTO_TCP))) { // PF_INET for packets
-//		perror("socket() failed");
-//		exit(EXIT_FAILURE);
-//	}
 
-	fprintf(stdout, "connect()\n");
+	fprintf(stdout, "connect()");
 	memset(&serveraddr, 0, sizeof(serveraddr));
 	serveraddr.sin_family = AF_INET; // AF_INET for addresses
 	serveraddr.sin_addr.s_addr = inet_addr(serverip);
 	serveraddr.sin_port = htons(atoi(port));
 	lothars__connect(fd_sock, ( struct sockaddr * )&serveraddr, sizeof(serveraddr));
-//	if (0 >
-//	    connect(fd_sock, ( struct sockaddr * )&serveraddr, sizeof(serveraddr))) {  
-//		perror("connect() failed");
-//		exit(EXIT_FAILURE);
-//	}
-	fprintf(stdout, "connect() - ok\n");
+	fprintf(stdout, " - ok\n");
 
-	fprintf(stdout, "send()\n");
-	echolen = strlen(text);
-	if (echolen != send(fd_sock, text, echolen, 0)) {  
-		perror("send() failed");
-		exit(EXIT_FAILURE);
-	}
-	fprintf(stdout, "send() - ok\n");
+	fprintf(stdout, "send()");
+	echolen = strlen(message);
+	lothars__send(fd_sock, message, echolen, 0);
+	fprintf(stdout, " - ok\n");
 
-	fprintf(stdout, "received: ");
 	while (nbytes < echolen) {
 		read = 0;
-		puts("recv()");
-		if (1 > (read = recv(fd_sock, buf, MAXSIZE - 1, 0))) { 
-			perror("recv() failed");
-			exit(EXIT_FAILURE);
-		}
-		fprintf(stdout, "recv() - ok\n");
+		fprintf(stdout, "recv()");
+		read = lothars__recv(fd_sock, buf, MAXLINE - 1, 0);
+		fprintf(stdout, " - ok\n");
 		nbytes += read;
 		buf[read] = '\0';
-		puts(buf);
+		fprintf(stdout, "received:\n%s\n\n", buf);
 	}
-	close(fd_sock); 
+
+	lothars__close(fd_sock);
 
 	fprintf(stdout, "READY.\n");
 	exit(EXIT_SUCCESS);
