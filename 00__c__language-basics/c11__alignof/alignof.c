0b0305149bda28ba3611e516992df2d409c17fd0
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-08 09:59:55 +0100

    basics: c11 alignof demo added

---
+++ 00__c__language-basics/c11__alignof/alignof.c
@@ -0,0 +1,70 @@
+/*
+  alignof (C11)
+
+
+  This operator is typically used through the convenience macro
+  alignof, which is provided in the header stdalign.h
+
+  Returns the alignment requirement of the type named by type-name. If
+  type-name is an array type, the result is the alignment requirement
+  of the array element type. The type-name cannot be function type or
+  an incomplete type.
+
+  The result is an integer constant of type size_t.
+
+  The operand is not evaluated (so external identifiers used in the
+  operand do not have to be defined).
+
+  If type-name is a VLA type, its size expression is not evaluated.
+
+  max_align_t is a type whose alignment requirement is at least as
+  strict (as large) as that of every scalar type.
+
+
+  Usage (Unix/Linux):
+  $ make
+  $ ./alignof.exe
+
+
+  References:
+  https://en.cppreference.com/w/c/language/_Alignof
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#include <inttypes.h> /* C11: fixed int types - PRIxPTR */
+
+#include <stdalign.h> /* C11: alignof */
+
+
+void header(char* txt)
+{
+	printf("\n\n");
+	printf(txt);
+	printf("\n\n");
+}
+
+int main(void)
+{
+	header("C11: type max_align_t");
+	size_t a = alignof(max_align_t);
+	printf("Alignment of max_align_t is %zu (%#zx)\n", a, a);
+
+	void *p = malloc(123);
+	printf("The address obtained from malloc(123) is %#" PRIxPTR"\n", (uintptr_t)p);
+	free(p);
+
+
+	header("C11: operator alignof()");
+	printf("Alignment of char = %zu\n", alignof(char));
+	printf("Alignment of max_align_t = %zu\n", alignof(max_align_t));
+	printf("alignof(float[10]) = %zu\n", alignof(float[10]));
+
+	printf("alignof(struct{char c; int n;}) = %zu\n", alignof(struct {char c; int n;}));
+
+
+	printf("\n");
+	printf("READY.\n");
+	exit(EXIT_SUCCESS);
+}
