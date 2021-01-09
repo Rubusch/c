fe24117e7fd4b9ac40fedc0c2f4f4925ef996bd3
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-09 21:14:02 +0100

    lib: lib_socket_demo - split in source and header for later testing

---
+++ lothars-devkit-library/lib_socket_demo.h
@@ -0,0 +1,69 @@
+#ifndef DEVKIT_LIB_SOCKET_DEMO
+#define DEVKIT_LIB_SOCKET_DEMO
+
+
+/*
+  Following could be derived from SOMAXCONN in <sys/socket.h>, but many
+  kernels still #define it as 5, while actually supporting many more
+*/
+#define LISTENQ  1024 /* listen queue size for listen(), can be set in env */
+#define MAXLINE  4096 /* max text line length */
+#define MAXFD 64 /* max file descriptors */
+#define BUFFSIZE 8192 /* buffer size for reads and writes */
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+// default file access permissions for new files
+
+#define DIR_MODE (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+// default permissions for new directories
+
+
+
+/* forwards */
+
+int connect_nonb(int, const struct sockaddr *, socklen_t, int);
+//int connect_timeo(int, const struct sockaddr *, socklen_t, int);
+void lothars__connect_timeo(int, const struct sockaddr *, socklen_t, int);
+
+int daemon_init(const char *, int);
+void daemon_inetd(const char *, int);
+
+void dg_cli(FILE*, int, const struct sockaddr *, socklen_t);
+void dg_echo(int, struct sockaddr*, socklen_t);
+
+int lothars__family_to_level(int);
+int lothars__socket_to_family(int);
+
+char* gf_time();
+
+struct addrinfo* lothars__host_serv(const char*, const char*, int, int);
+char** lothars__my_addrs(int*);
+
+void worker__echo_serv(int);
+void worker__echo_cli(FILE *, int);
+void worker__echo_serv_str(int);
+void worker__echo_serv_bin(int);
+void worker__echo_cli_bin(FILE*, int);
+int lothars__tcp_connect(const char*, const char*);
+int lothars__tcp_listen(const char*, const char*, socklen_t*);
+void tv_sub(struct timeval*, struct timeval*);
+int lothars__udp_client(const char*, const char*, struct sockaddr**, socklen_t*);
+int lothars__udp_connect(const char*, const char*);
+int lothars__udp_server(const char*, const char*, socklen_t*);
+int lothars__writable_timeo(int, int);
+int lothars__sock_bind_wild(int, int);
+
+char* lothars__sock_ntop(const struct sockaddr*, socklen_t);
+char* lothars__sock_ntop_host(const struct sockaddr*, socklen_t);
+
+int sockatmark(int);
+int lothars__readable_timeo(int, int);
+
+int sock_cmp_addr(const struct sockaddr*, const struct sockaddr*, socklen_t);
+int sock_cmp_port(const struct sockaddr*, const struct sockaddr*, socklen_t);
+int sock_get_port(const struct sockaddr*, socklen_t);
+void sock_set_addr(struct sockaddr*, socklen_t, const void*);
+void sock_set_port(struct sockaddr*, socklen_t, int);
+void sock_set_wild(struct sockaddr*, socklen_t);
+
+
+#endif /* DEVKIT_LIB_SOCKET_DEMO */
