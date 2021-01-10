b75799dfc7e17821ef17f1f72470021d184d1108
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-10 18:26:03 +0100

    lib: lib_route - split in source and header for later testing

--- lothars-devkit-library/lib_route.c
+++ lothars-devkit-library/lib_route.c
@@ -1,43 +1,7 @@
-/*
-  routing
-
-  routing socket support (still some if_nametoindex / if_indextoname
-  missing, supported by lib_socket)
-//*/
-// TODO rework                                             
-
-
-
-// forwards
-
-// function prototypes
-void  get_rtaddrs(int, struct sockaddr *, struct sockaddr **);
-const char* sock_masktop(struct sockaddr *, socklen_t);
-
-// wrapper functions
-char* lothars__net_rt_iflist(int, int, size_t *);
-char* lothars__net_rt_dump(int, int, size_t *);
-
-#define _sock_masktop(a,b) sock_masktop((a), (b))
-
-
-
-// implementation
+#include "lib_route.h"
 
 /*
-  Round up 'a' to next multiple of 'size', which must be a power of 2
 */
-#define ROUNDUP(a, size) (((a) & ((size)-1)) ? (1 + ((a) | ((size)-1))) : (a))
-
-/*
-  Step to next socket address structure. If sa_len is 0, assume it is
-  sizeof(u_long).
-*/
-#define NEXT_SA(ap) ap = (struct sockaddr *)				\
-		((caddr_t) ap + (ap->sa_len ? ROUNDUP(ap->sa_len, sizeof (u_long)) : \
-				 sizeof(u_long)))
-
-
 void get_rtaddrs(int addrs, struct sockaddr *sa, struct sockaddr **rti_info)
 {
 	int  idx;
---
+++ lothars-devkit-library/lib_route.h
@@ -0,0 +1,45 @@
+#ifndef DEVKIT_LIB_ROUTE
+#define DEVKIT_LIB_ROUTE
+
+
+/*
+  routing
+
+  routing socket support (still some if_nametoindex / if_indextoname
+  missing, supported by lib_socket)
+
+  ---
+  References:
+  Unix Network Programming, Stevens (1996)
+*/
+// TODO rework                                             
+
+// function prototypes
+void  get_rtaddrs(int, struct sockaddr *, struct sockaddr **);
+const char* sock_masktop(struct sockaddr *, socklen_t);
+
+// wrapper functions
+char* lothars__net_rt_iflist(int, int, size_t *);
+char* lothars__net_rt_dump(int, int, size_t *);
+
+#define _sock_masktop(a,b) sock_masktop((a), (b))
+
+
+
+// implementation
+
+/*
+  Round up 'a' to next multiple of 'size', which must be a power of 2
+*/
+#define ROUNDUP(a, size) (((a) & ((size)-1)) ? (1 + ((a) | ((size)-1))) : (a))
+
+/*
+  Step to next socket address structure. If sa_len is 0, assume it is
+  sizeof(u_long).
+*/
+#define NEXT_SA(ap) ap = (struct sockaddr *)				\
+		((caddr_t) ap + (ap->sa_len ? ROUNDUP(ap->sa_len, sizeof (u_long)) : \
+				 sizeof(u_long)))
+
+
+#endif /* DEVKIT_LIB_ROUTE */
