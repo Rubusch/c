c360f5b8493b78e7e34afa055e8dfb0e11942a2e
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-14 22:48:33 +0100

    ipc: signals - demo added

---
+++ 20__c__interprocess-communication/ipc__signals/lotharlib/console_input.h
@@ -0,0 +1,20 @@
+// console_input.h
+/*
+  console input functions
+//*/
+
+#ifndef CONSOLE_INPUT
+#define CONSOLE_INPUT
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void readdigit(unsigned int *, const char *);
+int isnumber(const char *, const unsigned int);
+void readnumber(unsigned int *, const unsigned int, const char *);
+void readlongnumber(unsigned long int *, const unsigned int, const char *);
+void readstring(char *, const unsigned int, const char *);
+
+#endif
