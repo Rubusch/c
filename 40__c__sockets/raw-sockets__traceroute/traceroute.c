25f87bdffe153a4dc3202206e8a549590f8cf2ae
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-21 10:03:40 +0100

    sockets: traceroute - cleanup

--- 40__c__sockets/raw-sockets__traceroute/traceroute.c
+++ 40__c__sockets/raw-sockets__traceroute/traceroute.c
@@ -11,9 +11,8 @@
   ---
   References:
   Unix Network Programming, Stevens (1996)
-
-  FIXME: no diference in options -m and -v -> check
 */
+// TODO check options -m and -v -> check
 
 #include "traceroute.h"
 
@@ -858,37 +857,37 @@ const char* icmpcode_v4(int code)
 
 	switch (code) {
 	case  0:
-		return("network unreachable");
+		return "network unreachable";
 	case  1:
-		return("host unreachable");
+		return "host unreachable";
 	case  2:
-		return("protocol unreachable");
+		return "protocol unreachable";
 	case  3:
-		return("port unreachable");
+		return "port unreachable";
 	case  4:
-		return("fragmentation required but DF bit set");
+		return "fragmentation required but DF bit set";
 	case  5:
-		return("source route failed");
+		return "source route failed";
 	case  6:
-		return("destination network unknown");
+		return "destination network unknown";
 	case  7:
-		return("destination host unknown");
+		return "destination host unknown";
 	case  8:
-		return("source host isolated (obsolete)");
+		return "source host isolated (obsolete)";
 	case  9:
-		return("destination network administratively prohibited");
+		return "destination network administratively prohibited";
 	case 10:
-		return("destination host administratively prohibited");
+		return "destination host administratively prohibited";
 	case 11:
-		return("network unreachable for TOS");
+		return "network unreachable for TOS";
 	case 12:
-		return("host unreachable for TOS");
+		return "host unreachable for TOS";
 	case 13:
-		return("communication administratively prohibited by filtering");
+		return "communication administratively prohibited by filtering";
 	case 14:
-		return("host recedence violation");
+		return "host recedence violation";
 	case 15:
-		return("precedence cutoff in effect");
+		return "precedence cutoff in effect";
 
 	default:
 		sprintf(errbuf, "[unknown code %d]", code);
