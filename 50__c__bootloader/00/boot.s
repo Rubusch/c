9167203f1480190f8b2d7830e3b05d5f31c85f15
Lothar Rubusch <l.rubusch@gmail.com>
2021-06-05 23:12:27 +0200

    booting: tutorial - gnu asm
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 50__c__bootloader/00/boot.s
@@ -0,0 +1,18 @@
+## taken from "50 lines of code" by Martin Splitt, 2018
+##
+## $ make
+##     as -o boot.o boot.s
+## $ ls -l
+##     total 12
+##     -rw-r--r-- 1 user user 696 Jun  5 23:04 boot.o
+##     -rw-r--r-- 1 user user 203 Jun  5 23:04 boot.s
+##     -rw-r--r-- 1 user user 198 Jun  5 23:00 Makefile
+##
+## as described boot.o will be already aroudn 696 Bytes, due to object
+## code and ELF format
+
+.code16           ## set the assembler to 16bit mode
+.global init      ## set the _start point
+
+init:
+        jmp init  ## just loop for the demo
