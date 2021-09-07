5b02264a3d7728aac033038cdb25a80d2f52168e
Lothar Rubusch <l.rubusch@gmail.com>
2021-09-07 13:15:42 +0100

    formatting: update
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 40__c__sockets/ioctl__get-ip/getip.c
+++ 40__c__sockets/ioctl__get-ip/getip.c
@@ -24,30 +24,30 @@ int main(int argc, char **argv) {
 	struct sockaddr_in *i;
 	int fd;
 
-	if(argc != 2) {
+	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <iface>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
-	if (strlen (argv[1]) >= sizeof ifa.ifr_name) {
-		fprintf (stderr, "%s is too long\n", argv[1]);
-		exit (EXIT_FAILURE);
+	if (strlen(argv[1]) >= sizeof ifa.ifr_name) {
+		fprintf(stderr, "%s is too long\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
 
-	strcpy (ifa.ifr_name, argv[1]);
+	strcpy(ifa.ifr_name, argv[1]);
 
 	if (0 > (fd = socket(AF_INET, SOCK_DGRAM, 0))) {
-		perror ("socket");
-		exit (EXIT_FAILURE);
+		perror("socket");
+		exit(EXIT_FAILURE);
 	}
 
 	if (0 > ioctl(fd, SIOCGIFADDR, &ifa)) {
-		perror ("ioctl");
-		exit (EXIT_FAILURE);
+		perror("ioctl");
+		exit(EXIT_FAILURE);
 	}
 
 	i = (struct sockaddr_in*)&ifa.ifr_addr;
-	puts (inet_ntoa(i->sin_addr));
+	puts(inet_ntoa(i->sin_addr));
 
 	exit(EXIT_SUCCESS);
 }
