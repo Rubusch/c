4daca5a0252df6e25a9e7ac2d38535b110450e6d
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-09 18:09:30 +0100

    attribute: demo to show the unused declaration

---
+++ 10__c__language-techniques/attribute__uninitialized/Hello.c
@@ -0,0 +1,30 @@
+/*
+  attribute usage
+
+  the demo shows how to use the unused attribute declaration;
+
+  without declaration:
+    gcc -g -Wall -Werror -std=c11 -pedantic   -c -o Hello.o Hello.c
+    Hello.c: In function ‘main’:
+    Hello.c:16:6: error: unused variable ‘idx’ [-Werror=unused-variable]
+      int idx;
+          ^~~
+    cc1: all warnings being treated as errors
+    make: *** [<builtin>: Hello.o] Error 1
+
+
+  @references:
+  http://unixwiz.net/techtips/gnu-c-attributes.html
+
+  @author: Lothar Rubusch <l.rubusch@gmail.com>
+  @license: GPLv3 2021
+ */
+
+#include <stdio.h>
+int main(int argc __attribute__((unused)), char *argv[]) /* apply to function arguments */
+{
+	int idx __attribute__((unused)); /* apply to variable declarations */
+
+	printf("Hello World!\n");
+	return 0;
+}
