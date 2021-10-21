fcf6c348ea610cd89b3ef915ceaf7ee42ada59dc
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-21 21:50:15 +0100

    signal: comments

--- 20__c__interprocess-communication/signal__alarm__via_sa_sigaction/alarm.c
+++ 20__c__interprocess-communication/signal__alarm__via_sa_sigaction/alarm.c
@@ -3,32 +3,51 @@
   Signals are similar to interrupts but are thrown by the software.
   Interrupts always come from the hardware, btw softinterrupts come
   from the CPU (= hardware!)
-
-  signal handler:
-  - make it short/simple
-  - proper signal masking, preferably use sigaction()
-  - be careful with "fault" signals, e.g. signals that indicate
-      program bugs (SIGBUS, SIGSEGV, SIGFPE)
-  - be careful with timers, don't abuse the VTALRM signal, if
-      you need to have more than one timer at a time, don't use
-      signals
-  - signals are NOT an event driven framework, this is abuse of signals
-      better use a select-based loop inside a network server or an
-      event loop of a windowing program directly
-
-  NB: flags
-SA_ONSTACK
-If set and an alternate signal stack has been declared with sigaltstack(), the signal shall be delivered to the calling process on that stack. Otherwise, the signal shall be delivered on the current stack.
-SA_RESETHAND
-If set, the disposition of the signal shall be reset to SIG_DFL and the SA_SIGINFO flag shall be cleared on entry to the signal handler.
-
-SA_RESTART
-This flag affects the behavior of interruptible functions; that is, those specified to fail with errno set to [EINTR]. If set, and a function specified as interruptible is interrupted by this signal, the function shall restart and shall not fail with [EINTR] unless otherwise specified. If the flag is not set, interruptible functions interrupted by this signal shall fail with errno set to [EINTR].
-SA_SIGINFO
-If cleared and the signal is caught, the signal-catching function shall be entered as:
-void func(int signo);
+  -> signal() is deprecated, use sigaction() and struct sigaction
 
 
+  struct sighandler_t
+
+  - sighandler_t->sa_handler() classical signal handler, takes one
+    argument the signal number, can also be set to
+       * SIG_DFL for the default action
+
+       * SIG_IGN to ignore this signal
+
+       * A pointer to a signal handling function; this function
+         receives the signal number as its only argument
+
+
+  - sighandler_t->sa_sigaction() signal handler, takes three
+    arguments, and allows for passing the siginfo_t (parent PID, etc)
+
+
+  Also check for sa_mask, allows to filter and block signals
+
+  sa_flags, set action to use
+       * SA_ONSTACK - If set and an alternate signal stack has been
+           declared with sigaltstack(), the signal shall be delivered
+           to the calling process on that stack. Otherwise, the signal
+           shall be delivered on the current stack.
+
+       * SA_RESETHAND - If set, the disposition of the signal shall be
+           reset to SIG_DFL and the SA_SIGINFO flag shall be cleared
+           on entry to the signal handler.
+
+       * SA_RESTART - This flag affects the behavior of interruptible
+           functions; that is, those specified to fail with errno set
+           to [EINTR]. If set, and a function specified as
+           interruptible is interrupted by this signal, the function
+           shall restart and shall not fail with [EINTR] unless
+           otherwise specified. If the flag is not set, interruptible
+           functions interrupted by this signal shall fail with errno
+           set to [EINTR].
+
+       * SA_SIGINFO - If cleared and the signal is caught, the
+           signal-catching function shall be entered as: void func(int
+           signo);
+
+       * SA_ONESHOT - The signalhandler executes only once
 */
 
 #include <stdio.h>
