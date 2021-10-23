09c6268a145975fcd1c154bc0457ccec5a6da0fe
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-23 18:11:06 +0100

    printf: 32 and 64 compatible printf
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 00__c__language-basics/printf__x32-x64/x64.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+
+int main(int argc, char *argv[])
+{
+	int64_t t = 777;
+
+	/*
+	  PRI.64 macros help to get rid of compiler warnings when
+	  switching from 32 bit systems to 64 bit, and vice versa
+	 */
+
+	printf("printf() as 'd': %" PRId64 "\n", t);
+
+	printf("printf() as 'u': %" PRIu64 "\n", t);
+
+	printf("printf() as 'x': 0x%" PRIx64 "\n", t);
+
+	fprintf(stderr, "READY.\n");
+	exit(EXIT_SUCCESS);
+}
