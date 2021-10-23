a4132b66637ac5dac32bb52ce219141a8d414741
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-23 12:09:53 +0100

    signal: syscall demo updated
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 20__c__interprocess-communication/signal__syscall/signal_syscall.c
@@ -0,0 +1,31 @@
+// signal_syscall.c
+/*
+  demonstrates the reaction on a sent signal
+  Send a SIGKILL (e.g. with htop) to end the prg!
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void catch_int(int sig)
+{
+	//  puts("Eh eh that tickles...\n");
+	printf("Eh eh that tickles...\n");
+	fflush(stdout);
+}
+
+int main(int argc, char **argv)
+{
+	struct sigaction sa;
+	sa.sa_handler = catch_int;
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+
+	// infinite loop
+	while (1)
+		pause();
+}
