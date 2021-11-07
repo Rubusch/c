c3395d0e676de285c18c09370b7ef3a9d480d074
Lothar Rubusch <l.rubusch@gmail.com>
2021-11-07 11:22:31 +0000

    algo: mergesort
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 60_algorithm/001_mergesort/unittest/test.c
@@ -0,0 +1,85 @@
+/*
+  cunit - tests
+ */
+#include "test.h"
+
+void test_sort(void)
+{
+	const int ARR_SIZE = 50;
+	int arr[ARR_SIZE];
+	int arr_orig[ARR_SIZE];
+	int upper = ARR_SIZE;
+	int lower = 0;
+	int idx, jdx;
+
+	srand(time(0));
+	for (idx=0; idx<ARR_SIZE; idx++) {
+		arr[idx] = (rand() % (upper - lower + 1)) + lower;
+		arr_orig[idx] = arr[idx];
+	}
+
+//*	// DEBUG
+	for (idx=0; idx<ARR_SIZE; idx++)
+		fprintf(stderr, " %d", arr[idx]);
+	fprintf(stderr, "\n");
+// */
+
+	sort(arr, ARR_SIZE);
+
+	for (idx=1; idx<ARR_SIZE; idx++) {
+		CU_ASSERT(arr[idx-1] <= arr[idx]);
+		for (jdx=0; jdx<ARR_SIZE; jdx++) {
+			if (arr[idx] == arr_orig[jdx]) {
+				arr_orig[jdx] = upper+1; // mark "found" by setting an impossible value
+				break;
+			}
+		}
+		if (jdx == ARR_SIZE){
+			CU_ASSERT(FALSE); // overrun, the element was
+					  // not in arr_orig, or not
+					  // that much of that element
+		}
+	}
+//*     // DEBUG
+	for (idx=0; idx<ARR_SIZE; idx++)
+		fprintf(stderr, " %d", arr[idx]);
+	fprintf(stderr, "\n");
+// */
+}
+
+int main()
+{
+	CU_pSuite pSuite = NULL;
+
+	/* initialize the CUnit test registry */
+	if (CUE_SUCCESS != CU_initialize_registry())
+		return CU_get_error();
+
+	/* add a suite to the registry */
+	pSuite = CU_add_suite("Suite_success", init_suite_success,
+			      clean_suite_success);
+	if (NULL == pSuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	/*add the tests to the suite */
+	TEST_start(pSuite, "sort", test_sort)
+	TEST_end();
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	fprintf(stderr, "\n");
+	CU_basic_show_failures(CU_get_failure_list());
+	fprintf(stderr, "\n\n");
+
+#if defined BASICTEST
+	CU_automated_run_tests();
+#else
+	CU_curses_run_tests();
+#endif
+
+	/* clean up registry and return */
+	CU_cleanup_registry();
+	return CU_get_error();
+}
