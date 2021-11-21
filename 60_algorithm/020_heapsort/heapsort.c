be78c4567372d135314fc720d3df85350e737a50
Lothar Rubusch <l.rubusch@gmail.com>
2021-11-21 21:23:48 +0000

    algo: malloc issue, WIP
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 60_algorithm/020_heapsort/heapsort.c
+++ 60_algorithm/020_heapsort/heapsort.c
@@ -194,7 +194,7 @@ int heap_extract_max(int **arr, int *size)
 	int max = heap_maximum(*arr, *size);
 /*
 	// realloc: reduce used memory
-//	int *tmp = realloc(*arr, ((*size-1) * sizeof(**arr)));
+//	int *tmp = realloc(*arr, ((*size - 1) * sizeof(**arr)));
 	int *tmp = realloc(*arr, ((*size - 1) * sizeof(*tmp)));
 	if (!tmp) {
 		if (NULL != tmp) {
@@ -204,8 +204,8 @@ int heap_extract_max(int **arr, int *size)
 		return -1;
 	}
 	tmp[*size] = -1;
-//	*arr = tmp;
-
+	*arr = tmp;
+// */
 	(*size)--;
 
 	heapify(*arr, 1, *size-1);
