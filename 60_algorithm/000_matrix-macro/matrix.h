c8d26faab3c8fd2854788ce93ba01dc78491bf01
Lothar Rubusch <l.rubusch@gmail.com>
2021-12-29 02:55:39 +0000

    algo: matrix macro-ized version for demos
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 60_algorithm/000_matrix-macro/matrix.h
@@ -0,0 +1,151 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+# ifndef DEBUG
+#  define DEBUG 1
+# endif /* DEBUG */
+
+
+/*
+  part 1: the demo
+ */
+#define create_matrix(NAME, TYPE, MATRIX_NAME_SIZE, MATRIX_FAILURE_FUNC) \
+									\
+	typedef struct NAME##_matrix_s					\
+	{								\
+		char name[MATRIX_NAME_SIZE];				\
+		TYPE **m;						\
+		int nrows;						\
+		int ncols;						\
+	} NAME##_matrix_t;						\
+	typedef NAME##_matrix_t* NAME##_matrix_p;			\
+									\
+	static inline NAME##_matrix_p NAME##_matrix_create(const char *name, \
+							   int nrows, int ncols); \
+									\
+	static inline void NAME##_matrix_init_all(NAME##_matrix_p mat, int value); \
+									\
+	static inline void NAME##_matrix_init_row(NAME##_matrix_p mat, int rowidx, \
+						  int* vals, int vals_size); \
+									\
+	static inline void NAME##_matrix_destroy(NAME##_matrix_p mat);	\
+									\
+	NAME##_matrix_p NAME##_matrix_create(const char *name, int nrows, int ncols) \
+	{								\
+		NAME##_matrix_p mat = malloc(sizeof(*mat));		\
+		if (!mat) {						\
+			MATRIX_FAILURE_FUNC("allocation failed");	\
+		}							\
+									\
+		mat->m = malloc(nrows * sizeof(*mat->m));		\
+		for (int idx = 0; idx < nrows; idx++) {			\
+			mat->m[idx] = malloc(ncols * sizeof(*mat->m));	\
+			for (int jdx = 0; jdx < ncols; jdx++) {		\
+				mat->m[idx][jdx] = (TYPE) 0;		\
+			}						\
+		}							\
+									\
+		if (strlen(name) >= MATRIX_NAME_SIZE) {			\
+			MATRIX_FAILURE_FUNC("matrix name overrun");	\
+		}							\
+		memset(mat->name, '\0', (MATRIX_NAME_SIZE-1) * sizeof(*mat->name)); \
+		strncpy(mat->name, name, strlen(name));			\
+		mat->name[MATRIX_NAME_SIZE-1] = '\0';			\
+		mat->ncols = ncols;					\
+		mat->nrows = nrows;					\
+									\
+		return mat;						\
+	}								\
+									\
+	void NAME##_matrix_destroy(NAME##_matrix_p mat)			\
+	{								\
+		if (!mat) {						\
+			return;						\
+		}							\
+									\
+		for (int idx = 0; idx < mat->nrows; idx++) {		\
+			free(mat->m[idx]);				\
+		}							\
+		free(mat->m);						\
+		free(mat);						\
+	}								\
+									\
+	void NAME##_matrix_init_all(NAME##_matrix_p mat, TYPE value)	\
+	{								\
+		if (!mat) return;					\
+		for (int idx = 0; idx < mat->nrows; idx++) {		\
+			for (int jdx = 0; jdx < mat->ncols; jdx++) {	\
+				mat->m[idx][jdx] = value;		\
+			}						\
+		}							\
+	}								\
+									\
+	void NAME##_matrix_init_row(NAME##_matrix_p mat, int rowidx,	\
+				    TYPE* vals, int vals_size)		\
+	{								\
+		if (!mat) return;					\
+		if (rowidx >= mat->nrows) return;			\
+		if (vals_size != mat->ncols) return;			\
+									\
+		for (int idx = 0; idx < mat->ncols; idx++) {		\
+			mat->m[rowidx][idx] = vals[idx];		\
+		}							\
+	}								\
+
+
+
+/*
+  part 2
+
+  the following functions can be provided and in case reimplemented
+  for other types - only matrix_failure (or similar) must be provided
+ * /
+void matrix_debug(const char* format, ...);
+void matrix_failure(const char* format, ...);
+void matrix_print(matrix_p mat);
+
+void matrix_debug(const char* format, ...)
+{
+#ifdef DEBUG
+	va_list arglist;
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+#endif
+}
+
+void matrix_failure(const char* format, ...)
+{
+	perror("failed!");
+	va_list arglist;
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+	exit(EXIT_FAILURE);
+}
+
+void matrix_print(matrix_p mat)
+{
+	if (!mat) {
+		return;
+	}
+	matrix_debug("\n%s =\n", mat->name);
+	for (int idx = 0; idx < mat->nrows; idx++) {
+		for (int jdx = 0; jdx < mat->ncols; jdx++) {
+			matrix_debug("%d ", mat->m[idx][jdx]);
+		}
+		matrix_debug("\n");
+	}
+}
+// */
+
+
+/*
+  part 3
+
+ demo instantiation for the unittests (usage)
+* /
+create_matrix(my, int, 16, matrix_failure);
+// */
+
+#endif /* MATRIX_H */
---
+++ 60_algorithm/000_matrix-macro/unittest/test.c
@@ -0,0 +1,277 @@
+/*
+  cunit - tests
+ */
+
+#include "matrix.h"
+
+#include "test.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+void matrix_debug(const char* format, ...)
+{
+#ifdef DEBUG
+	va_list arglist;
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+#endif
+}
+
+void matrix_failure(const char* format, ...)
+{
+	perror("failed!");
+	va_list arglist;
+	va_start(arglist, format);
+	vprintf(format, arglist);
+	va_end(arglist);
+	exit(EXIT_FAILURE);
+}
+
+void matrix_print(my_matrix_p mat)
+{
+	if (!mat) {
+		return;
+	}
+	matrix_debug("\n%s =\n", mat->name);
+	for (int idx = 0; idx < mat->nrows; idx++) {
+		for (int jdx = 0; jdx < mat->ncols; jdx++) {
+			matrix_debug("%d ", mat->m[idx][jdx]);
+		}
+		matrix_debug("\n");
+	}
+}
+
+// tests
+void test_matrix_simple(void)
+{
+	my_matrix_p mat = NULL;
+	CU_ASSERT(NULL == mat);
+	mat = my_matrix_create("MATRIX", 10, 10);
+	CU_ASSERT(NULL != mat);
+	CU_ASSERT(0 == strcmp("MATRIX", mat->name));
+	matrix_print(mat);
+
+	CU_ASSERT(0 == mat->m[ 0][ 0]);
+	CU_ASSERT(0 == mat->m[ 0][ 1]);
+	CU_ASSERT(0 == mat->m[ 0][ 2]);
+	CU_ASSERT(0 == mat->m[ 0][ 3]);
+	CU_ASSERT(0 == mat->m[ 0][ 4]);
+	CU_ASSERT(0 == mat->m[ 0][ 5]);
+	CU_ASSERT(0 == mat->m[ 0][ 6]);
+	CU_ASSERT(0 == mat->m[ 0][ 7]);
+	CU_ASSERT(0 == mat->m[ 0][ 8]);
+	CU_ASSERT(0 == mat->m[ 0][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 1][ 0]);
+	CU_ASSERT(0 == mat->m[ 1][ 1]);
+	CU_ASSERT(0 == mat->m[ 1][ 2]);
+	CU_ASSERT(0 == mat->m[ 1][ 3]);
+	CU_ASSERT(0 == mat->m[ 1][ 4]);
+	CU_ASSERT(0 == mat->m[ 1][ 5]);
+	CU_ASSERT(0 == mat->m[ 1][ 6]);
+	CU_ASSERT(0 == mat->m[ 1][ 7]);
+	CU_ASSERT(0 == mat->m[ 1][ 8]);
+	CU_ASSERT(0 == mat->m[ 1][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 2][ 0]);
+	CU_ASSERT(0 == mat->m[ 2][ 1]);
+	CU_ASSERT(0 == mat->m[ 2][ 2]);
+	CU_ASSERT(0 == mat->m[ 2][ 3]);
+	CU_ASSERT(0 == mat->m[ 2][ 4]);
+	CU_ASSERT(0 == mat->m[ 2][ 5]);
+	CU_ASSERT(0 == mat->m[ 2][ 6]);
+	CU_ASSERT(0 == mat->m[ 2][ 7]);
+	CU_ASSERT(0 == mat->m[ 2][ 8]);
+	CU_ASSERT(0 == mat->m[ 2][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 3][ 0]);
+	CU_ASSERT(0 == mat->m[ 3][ 1]);
+	CU_ASSERT(0 == mat->m[ 3][ 2]);
+	CU_ASSERT(0 == mat->m[ 3][ 3]);
+	CU_ASSERT(0 == mat->m[ 3][ 4]);
+	CU_ASSERT(0 == mat->m[ 3][ 5]);
+	CU_ASSERT(0 == mat->m[ 3][ 6]);
+	CU_ASSERT(0 == mat->m[ 3][ 7]);
+	CU_ASSERT(0 == mat->m[ 3][ 8]);
+	CU_ASSERT(0 == mat->m[ 3][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 4][ 0]);
+	CU_ASSERT(0 == mat->m[ 4][ 1]);
+	CU_ASSERT(0 == mat->m[ 4][ 2]);
+	CU_ASSERT(0 == mat->m[ 4][ 3]);
+	CU_ASSERT(0 == mat->m[ 4][ 4]);
+	CU_ASSERT(0 == mat->m[ 4][ 5]);
+	CU_ASSERT(0 == mat->m[ 4][ 6]);
+	CU_ASSERT(0 == mat->m[ 4][ 7]);
+	CU_ASSERT(0 == mat->m[ 4][ 8]);
+	CU_ASSERT(0 == mat->m[ 4][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 5][ 0]);
+	CU_ASSERT(0 == mat->m[ 5][ 1]);
+	CU_ASSERT(0 == mat->m[ 5][ 2]);
+	CU_ASSERT(0 == mat->m[ 5][ 3]);
+	CU_ASSERT(0 == mat->m[ 5][ 4]);
+	CU_ASSERT(0 == mat->m[ 5][ 5]);
+	CU_ASSERT(0 == mat->m[ 5][ 6]);
+	CU_ASSERT(0 == mat->m[ 5][ 7]);
+	CU_ASSERT(0 == mat->m[ 5][ 8]);
+	CU_ASSERT(0 == mat->m[ 5][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 6][ 0]);
+	CU_ASSERT(0 == mat->m[ 6][ 1]);
+	CU_ASSERT(0 == mat->m[ 6][ 2]);
+	CU_ASSERT(0 == mat->m[ 6][ 3]);
+	CU_ASSERT(0 == mat->m[ 6][ 4]);
+	CU_ASSERT(0 == mat->m[ 6][ 5]);
+	CU_ASSERT(0 == mat->m[ 6][ 6]);
+	CU_ASSERT(0 == mat->m[ 6][ 7]);
+	CU_ASSERT(0 == mat->m[ 6][ 8]);
+	CU_ASSERT(0 == mat->m[ 6][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 7][ 0]);
+	CU_ASSERT(0 == mat->m[ 7][ 1]);
+	CU_ASSERT(0 == mat->m[ 7][ 2]);
+	CU_ASSERT(0 == mat->m[ 7][ 3]);
+	CU_ASSERT(0 == mat->m[ 7][ 4]);
+	CU_ASSERT(0 == mat->m[ 7][ 5]);
+	CU_ASSERT(0 == mat->m[ 7][ 6]);
+	CU_ASSERT(0 == mat->m[ 7][ 7]);
+	CU_ASSERT(0 == mat->m[ 7][ 8]);
+	CU_ASSERT(0 == mat->m[ 7][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 8][ 0]);
+	CU_ASSERT(0 == mat->m[ 8][ 1]);
+	CU_ASSERT(0 == mat->m[ 8][ 2]);
+	CU_ASSERT(0 == mat->m[ 8][ 3]);
+	CU_ASSERT(0 == mat->m[ 8][ 4]);
+	CU_ASSERT(0 == mat->m[ 8][ 5]);
+	CU_ASSERT(0 == mat->m[ 8][ 6]);
+	CU_ASSERT(0 == mat->m[ 8][ 7]);
+	CU_ASSERT(0 == mat->m[ 8][ 8]);
+	CU_ASSERT(0 == mat->m[ 8][ 9]);
+
+	CU_ASSERT(0 == mat->m[ 9][ 0]);
+	CU_ASSERT(0 == mat->m[ 9][ 1]);
+	CU_ASSERT(0 == mat->m[ 9][ 2]);
+	CU_ASSERT(0 == mat->m[ 9][ 3]);
+	CU_ASSERT(0 == mat->m[ 9][ 4]);
+	CU_ASSERT(0 == mat->m[ 9][ 5]);
+	CU_ASSERT(0 == mat->m[ 9][ 6]);
+	CU_ASSERT(0 == mat->m[ 9][ 7]);
+	CU_ASSERT(0 == mat->m[ 9][ 8]);
+	CU_ASSERT(0 == mat->m[ 9][ 9]);
+
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int arr_size = sizeof(arr) / sizeof(*arr);
+	my_matrix_init_row(mat, 7, arr, arr_size);
+	matrix_print(mat);
+
+	CU_ASSERT(1 == mat->m[ 7][ 0]);
+	CU_ASSERT(2 == mat->m[ 7][ 1]);
+	CU_ASSERT(3 == mat->m[ 7][ 2]);
+	CU_ASSERT(4 == mat->m[ 7][ 3]);
+	CU_ASSERT(5 == mat->m[ 7][ 4]);
+	CU_ASSERT(6 == mat->m[ 7][ 5]);
+	CU_ASSERT(7 == mat->m[ 7][ 6]);
+	CU_ASSERT(8 == mat->m[ 7][ 7]);
+	CU_ASSERT(9 == mat->m[ 7][ 8]);
+	CU_ASSERT(10 == mat->m[ 7][ 9]);
+
+	my_matrix_destroy(mat);
+	mat = NULL;
+}
+
+void test_matrix_multiple(void)
+{
+	my_matrix_p A = NULL;
+	A = my_matrix_create("A", 3, 2);
+	int a0[] = {1,2}; my_matrix_init_row(A, 0, a0, 2);
+	int a1[] = {3,4}; my_matrix_init_row(A, 1, a1, 2);
+	int a2[] = {5,6}; my_matrix_init_row(A, 2, a2, 2);
+
+	CU_ASSERT(1 == A->m[ 0][ 0]);
+	CU_ASSERT(2 == A->m[ 0][ 1]);
+	CU_ASSERT(3 == A->m[ 1][ 0]);
+	CU_ASSERT(4 == A->m[ 1][ 1]);
+	CU_ASSERT(5 == A->m[ 2][ 0]);
+	CU_ASSERT(6 == A->m[ 2][ 1]);
+
+	my_matrix_p B = NULL;
+	B = my_matrix_create("B", 2, 3);
+	int b0[] = {1,2,3}; my_matrix_init_row(B, 0, b0, 3);
+	int b1[] = {4,5,6}; my_matrix_init_row(B, 1, b1, 3);
+
+	CU_ASSERT(1 == B->m[ 0][ 0]);
+	CU_ASSERT(2 == B->m[ 0][ 1]);
+	CU_ASSERT(3 == B->m[ 0][ 2]);
+	CU_ASSERT(4 == B->m[ 1][ 0]);
+	CU_ASSERT(5 == B->m[ 1][ 1]);
+	CU_ASSERT(6 == B->m[ 1][ 2]);
+
+	matrix_print(A);
+	matrix_print(B);
+
+	my_matrix_destroy(A);
+	my_matrix_destroy(B);
+}
+
+void test_matrix_init(void)
+{
+	my_matrix_p M = NULL;
+	M = my_matrix_create("M", 3, 3);
+	my_matrix_init_all(M, 123);
+	matrix_print(M);
+
+	CU_ASSERT(123 == M->m[ 0][ 0]);
+	CU_ASSERT(123 == M->m[ 0][ 1]);
+	CU_ASSERT(123 == M->m[ 0][ 2]);
+	CU_ASSERT(123 == M->m[ 1][ 0]);
+	CU_ASSERT(123 == M->m[ 1][ 1]);
+	CU_ASSERT(123 == M->m[ 1][ 2]);
+	CU_ASSERT(123 == M->m[ 2][ 0]);
+	CU_ASSERT(123 == M->m[ 2][ 1]);
+	CU_ASSERT(123 == M->m[ 2][ 2]);
+}
+
+int main(void)
+{
+	CU_pSuite pSuite = NULL;
+
+	/* initialize the CUnit test registry */
+	if (CUE_SUCCESS != CU_initialize_registry())
+		return CU_get_error();
+
+	/* add a suite to the registry */
+	pSuite = CU_add_suite("dynamic-programming", init_suite_matrix,
+			      clean_suite_matrix);
+	if (NULL == pSuite) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	/* utilities */
+	TEST_start(pSuite, "matrix struct", test_matrix_simple)
+ 		TEST_append(pSuite, "matrix multiple",
+			    test_matrix_multiple) // */
+		TEST_append(pSuite, "matrix init",
+			    test_matrix_init) // */
+	TEST_end();
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+#if defined BASICTEST
+	CU_basic_run_tests();
+#else
+	CU_curses_run_tests();
+#endif
+	fprintf(stderr, "\n");
+	CU_basic_show_failures(CU_get_failure_list());
+	fprintf(stderr, "\n\n");
+
+	CU_cleanup_registry();
+	return CU_get_error();
+}
