29c09948695faf5adeffdaf903bc46e13a0cba21
Lothar Rubusch <l.rubusch@gmail.com>
2022-04-06 22:12:10 +0100

    system: time - timer
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 99_linux/time__timer/timer.c
@@ -0,0 +1,52 @@
+// timer.c
+/*
+  @author: Lothar Rubusch
+  @email: L.Rubusch@gmx.ch
+  @license: EPL
+  @2013-october-22
+
+  sets up a timer on a linux system, the timer ticks down 250ms sec and prints
+  out a message
+
+  uses signals and sigaction()
+//*/
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+void timer_handler(int signum)
+{
+	static int cnt = 0;
+	printf("timer expired %d times\n", ++cnt);
+}
+
+int main()
+{
+	struct sigaction sa;
+	struct itimerval timer;
+
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = &timer_handler;
+	sigaction(SIGVTALRM, &sa, NULL);
+
+	// init
+	timer.it_value.tv_sec = 0;
+
+	// set to 250ms expiry time
+	timer.it_value.tv_usec = 250000;
+	timer.it_interval.tv_sec = 0;
+
+	// and every 250ms after that
+	timer.it_interval.tv_usec = 250000;
+
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);
+
+	// infinite!
+	while (1)
+		;
+
+	exit(EXIT_SUCCESS);
+}
