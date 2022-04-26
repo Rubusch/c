6f83d1324347533797752c3b0d5deee9233d9ee5
Lothar Rubusch <l.rubusch@gmail.com>
2022-04-26 21:25:02 +0100

    ipc: sigaltstack reworked and fixed
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 20_interprocess-communication/signal__sigaltstack/sigaltstack.c
+++ 20_interprocess-communication/signal__sigaltstack/sigaltstack.c
@@ -3,9 +3,19 @@
   $ ulimit -s unlimited
   $ ./sigaltstack.elf
       ...many intervening lines of output removed...
+    call   18 - top of stack near 0xbe92a698
+    overflow_stack()...
+    call   19 - top of stack near 0xbe911fe8
+    overflow_stack()...
+    call   20 - top of stack near 0xbe8f9938
+
+--> catches SIGSEGV
+    SEGFAULT!
+    caught signal 11 (Segmentation fault)
+    top of handler stack near        0x1d70e84
 
 
-  demostrates sigaltstack()
+  demostrates sigaltstack(), catches SIGSEGV on the signal stack
 
 
   references:
@@ -25,19 +35,28 @@
 #include <signal.h>
 #include <unistd.h>
 
+#ifndef SIGSTKSZ
+# define SIGSTKSZ 100
+#endif
+
 static void
 sigsegv_handler(int sig)
 {
-	int x;
+	// UNSAFE: write() might work here
+	write(STDERR_FILENO, "SEGFAULT!\n", 10);
 
 	// UNSAFE: don't use stdio functions inside signalhandler
-	fprintf(stderr, "caught signal %d (%s)\n",
+	// NB: "stderr" won't work here!!!
+	fprintf(stdout, "caught signal %d (%s)\n",
 		sig, strsignal(sig));
-	fprintf(stderr, "top of handler stack near\t%10p\n",
+
+	int x; // needed to show +/- address range where we are...
+	fprintf(stdout, "top of handler stack near\t%10p\n",
 		(void*) &x);
 	fflush(NULL);
 
-	_exit(EXIT_FAILURE); // NB: don't use exit(), prefer _exit() in signal handler
+	_exit(EXIT_FAILURE);
+        // NB: don't use exit(), prefer _exit() in signal handler
 }
 
 static void
@@ -58,7 +77,6 @@ main(int argc, char* argv[])
 	struct sigaction sa;
 	int idx;
 
-
 	fprintf(stderr, "top of standard stack is near %10p\n",
 		(void*) &idx);
 
@@ -66,25 +84,24 @@ main(int argc, char* argv[])
 
 	sigstack.ss_sp = malloc(SIGSTKSZ);
 	if (NULL == sigstack.ss_sp) {
-		fprintf(stderr, "malloc() failed\n");
+		perror("malloc() failed");
 		exit(EXIT_FAILURE);
 	}
 
 	sigstack.ss_size = SIGSTKSZ;
 	sigstack.ss_flags = 0;
 	if (-1 == sigaltstack(&sigstack, NULL)) {
-		fprintf(stderr, "sigaltstack() failed\n");
+		perror("sigaltstack() failed");
 		exit(EXIT_FAILURE);
 	}
 
 	fprintf(stderr, "alternate stack is at\t%10p - %p\n",
 		sigstack.ss_sp, (char*) sbrk(0) - 1);
 
-	sa.sa_handler = sigsegv_handler; // establish handler for sigsegv
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_ONSTACK;
+	sa.sa_handler = sigsegv_handler; // establish handler for sigsegv
+	sa.sa_flags = SA_ONSTACK;  
 	if (-1 == sigaction(SIGSEGV, &sa, NULL)) { // handler uses alternate stack
-		// NB: SIGSEGV probably can't be caught
 		perror("sigaction() failed");
 		exit(EXIT_FAILURE);
 	}
