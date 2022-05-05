c2dfabdb9cc4be0712286e90ed1f4c1a91a8df14
Lothar Rubusch <l.rubusch@gmail.com>
2022-05-05 23:03:46 +0100

    sockets: brush up ping implementation
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 40_sockets/raw-sockets__ping/ping.c
+++ 40_sockets/raw-sockets__ping/ping.c
@@ -382,6 +382,8 @@ void proc_v4(char *ptr, ssize_t len, struct msghdr *msg,
 	struct icmp *icmp = NULL;
 	struct timeval *tv_send = NULL;
 
+	setbuf(stdout, NULL); // make stdout unbuffered
+
 	// init with start of IP header
 	ip = (struct ip *)ptr;
 
@@ -417,16 +419,17 @@ void proc_v4(char *ptr, ssize_t len, struct msghdr *msg,
 		rtt = tv_recv->tv_sec * 1000.0 + tv_recv->tv_usec / 1000.0;
 
 		fprintf(stdout,
-			"%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f ms\n",
+			"%d bytes from %s: seq=%u, ttl=%d, rtt=%.3f ms",
 			icmplen,
 			lothars__sock_ntop_host(proto->sa_recv, proto->salen),
 			icmp->icmp_seq, ip->ip_ttl, rtt);
 
-	} else if (verbose) {
-		fprintf(stdout, " %d bytes from %s: type=%d, code=%d\n",
-			icmplen,
-			lothars__sock_ntop_host(proto->sa_recv, proto->salen),
+	}
+	if (verbose) {
+		fprintf(stdout, ", type=%d, code=%d\n",
 			icmp->icmp_type, icmp->icmp_code);
+	} else {
+		fprintf(stderr, "\n");
 	}
 }
 
