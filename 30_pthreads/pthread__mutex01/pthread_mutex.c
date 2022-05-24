d47970233fe84b313ca560cf5cf73b53facd1e31
Lothar Rubusch <l.rubusch@gmail.com>
2022-05-24 21:20:35 +0100

    pthreads: mutex demos - formatting and comments
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 30_pthreads/pthread__mutex01/pthread_mutex.c
+++ 30_pthreads/pthread__mutex01/pthread_mutex.c
@@ -1,22 +1,26 @@
-// pthread_mutex.c
 /*
-  MUTEXES (used generally for synchronizing two threads, can provoke deadlocks
-with more threads)
+  MUTEXES (used generally for synchronizing two threads, can provoke
+  deadlocks with more threads)
 
 
 
-  pthread_self() - returns the unique, system assigned thread ID of the calling
-thread
+  pthread_self() - returns the unique, system assigned thread ID of
+                              the calling thread
 
   pthread_equal(thread_id1, thread_id2) - compares two thread IDs
 
-  pthread_once(once_control, init_function) - executes the given init_function
-exactly once in a process once_control is a synchronization control structure
-that requires initialization prior to calling pthread_once(), e.g.:
-pthread_once_t once_control = PTHREAD_ONCE_INIT;
+  pthread_once(once_control, init_function) - executes the given
+                              init_function exactly once in a process
+                              once_control is a synchronization
+                              control structure that requires
+                              initialization prior to calling
+                              pthread_once(), e.g.:
 
-  pthread_yield() - forces the calling thread to relinquish use of its processor
-and to wait in the run queue before it is scheduled again
+                              pthread_once_t once_control = PTHREAD_ONCE_INIT;
+
+  pthread_yield() - forces the calling thread to relinquish use of its
+                              processor and to wait in the run queue
+                              before it is scheduled again
 
   pthread_mutex_init(mutex, attr) - creates a mutex object dynamically
   pthread_mutex_destroy(mutex)    - destroys a mutex object dynamically
@@ -34,8 +38,9 @@ and to wait in the run queue before it is scheduled again
   6. another thread acquires the mutex and repeats the process
   7. finally the mutex is destroyed
 
-  mutex variables must be declared with type pthrea_mutex_t and must be
-initialized befofe they can be used, mutex variables initially are UNLOCKED
+  mutex variables must be declared with type pthrea_mutex_t and must
+  be initialized befofe they can be used, mutex variables initially
+  are UNLOCKED
 
   - mutex variables can be declared statically
   pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
@@ -47,17 +52,20 @@ initialized befofe they can be used, mutex variables initially are UNLOCKED
 
   pthread_mutexattr_t, can be initialized by
   - NULL (defaults)
-  - protocol (specifies the protocol used to prevent priority inversions for a
-mutex)
+  - protocol (specifies the protocol used to prevent priority
+    inversions for a mutex)
   - prioceiling (specifies the priority ceiling of a mutex)
   - process-shared (specifies the process sharing of a mutex)
 
 
-  pthread_mutex_lock(mutex)    - used by a thread to acquire a lock on the
-specified mutex variable this call will block other calls for the same mutex
+  pthread_mutex_lock(mutex) - used by a thread to acquire a lock on
+                                 the specified mutex variable this
+                                 call will block other calls for the
+                                 same mutex
 
-  pthread_mutex_trylock(mutex) - if the mutex is already locked this call will
-return with a busy error code
+  pthread_mutex_trylock(mutex) - if the mutex is already locked this
+                                 call will return with a busy error
+                                 code
 
   pthread_mutex_unlock(mutex)  - unlocks a mutex if called by the owner of the
 mutex, returns an error if the mutex is owned by another thread or if the mutex
@@ -72,9 +80,10 @@ already was unlocked
 #include <pthread.h>
 
 /*
-  structure contains necessary information to allow the function dotprod() to
-access its input data and place its output into the structure
-//*/
+  structure contains necessary information to allow the function
+  dotprod() to access its input data and place its output into the
+  structure
+*/
 typedef struct {
 	double *a;
 	double *b;
@@ -84,7 +93,7 @@ typedef struct {
 
 /*
   define globally accessible variables to share and a mutex
-//*/
+*/
 #define NUMTHRDS 4
 #define VECLEN 100
 
@@ -98,11 +107,12 @@ pthread_t callThd[NUMTHRDS];
 pthread_mutex_t mutexsum;
 
 /*
-  the thread function, all input to this function is obtained from a structure
-of type DOTDATA and all output from this function is written into this
-structure. when a thread is created we pas a single argument to the activated
-function - typically this argument is a thread number
-//*/
+  the thread function, all input to this function is obtained from a
+  structure of type DOTDATA and all output from this function is
+  written into this structure. when a thread is created we pas a
+  single argument to the activated function - typically this argument
+  is a thread number
+*/
 void *dotprod(void *arg)
 {
 	// local variables
@@ -117,18 +127,18 @@ void *dotprod(void *arg)
 	yVal = dotstr.b;
 
 	/*
-    perform the dot product and assign result to the appropriate variable in the
-  structure
-  //*/
+	  perform the dot product and assign result to the appropriate
+	  variable in the structure
+	*/
 	mysum = 0;
 	for (idx = start; idx < end; ++idx) {
 		mysum += (xVal[idx] * yVal[idx]);
 	}
 
 	/*
-    lock the mutex prior to updating the value in the shared structure and
-  unlock it upon updating
-  //*/
+	  lock the mutex prior to updating the value in the shared structure and
+	  unlock it upon updating
+	*/
 	pthread_mutex_lock(&mutexsum);
 	dotstr.sum += mysum;
 	pthread_mutex_unlock(&mutexsum);
@@ -136,10 +146,11 @@ void *dotprod(void *arg)
 }
 
 /*
-  the main program creates threads which do all the work and then print out
-result upon completion
-//*/
-int main(int argc, char **argv)
+  the main program creates threads which do all the work and then
+  print out result upon completion
+*/
+int
+main(int argc, char *argv[])
 {
 	int idx;
 	double *aVal = NULL, *bVal = NULL;
@@ -176,27 +187,29 @@ int main(int argc, char **argv)
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	for (idx = 0; idx < NUMTHRDS; ++idx) {
 		/*
-      each thread works on a different set of data, the offset is specified by
-    idx, the size of data for each thread is indicated by VECLEN
-    //*/
+		  each thread works on a different set of data, the
+		  offset is specified by idx, the size of data for
+		  each thread is indicated by VECLEN
+		*/
 		pthread_create(&callThd[idx], &attr, dotprod,
 			       (void *)(intptr_t)idx);
 	}
 	pthread_attr_destroy(&attr);
 
 	/*
-    wait on the other threads
-  //*/
+	  wait on the other threads
+	*/
 	for (idx = 0; idx < NUMTHRDS; ++idx) {
 		pthread_join(callThd[idx], &status);
 	}
 
 	/*
-    after joining, print out hte results and clean up
-  //*/
+	  after joining, print out hte results and clean up
+	*/
 	printf("sum = %f\n", dotstr.sum);
 	free(aVal);
 	free(bVal);
+
 	pthread_mutex_destroy(&mutexsum);
 	pthread_exit(NULL);
 }
--- 30_pthreads/pthread__mutex02/pthread_mutex.c
+++ 30_pthreads/pthread__mutex02/pthread_mutex.c
@@ -9,41 +9,55 @@
 
 #include <pthread.h>
 
-void *thread_function();
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int counter = 0;
 
-int main(int argc, char **argv)
+void*
+thread_function()
+{
+	int ret;
+
+	ret = pthread_mutex_lock(&mutex);
+	if (0 != ret) {
+		fprintf(stderr, "pthread_mutex_lock() failed\n");
+		_exit(EXIT_FAILURE);
+	}
+
+	++counter;
+	printf("counter value: %d\n", counter);
+
+	ret = pthread_mutex_unlock(&mutex);
+	if (0 != ret) {
+		fprintf(stderr, "pthread_mutex_unlock() failed\n");
+		_exit(EXIT_FAILURE);
+	}
+
+	pthread_exit(NULL);
+}
+
+int
+main(int argc, char* argv[])
 {
 	int rc1 = 0, rc2 = 0;
 	pthread_t thread1, thread2;
 
-	// create independent threads each of which will execute the thread_function()
-	if (0 !=
-	    (rc1 = pthread_create(&thread1, NULL, &thread_function, NULL))) {
+	// create independent threads each of which will execute the
+	// thread_function()
+	rc1 = pthread_create(&thread1, NULL, &thread_function, NULL);
+	if (0 != rc1) {
 		perror("pthread_create() failed");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-	if (0 !=
-	    (rc2 = pthread_create(&thread2, NULL, &thread_function, NULL))) {
+
+	rc2 = pthread_create(&thread2, NULL, &thread_function, NULL);
+	if (0 != rc2) {
 		perror("pthread_create() failed");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
 	// wait until threads are completed
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
-
-	pthread_exit(NULL);
-}
-
-void *thread_function()
-{
-	pthread_mutex_lock(&mutex);
-	++counter;
-	printf("counter value: %d\n", counter);
-	pthread_mutex_unlock(&mutex);
 
 	pthread_exit(NULL);
 }
