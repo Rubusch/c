4a5b41502481c1524cb46e3b2d47b43da6492fab
Lothar Rubusch <l.rubusch@gmail.com>
2022-08-07 21:34:09 +0100

    system: mmap - comments
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 70_system/mmap__cat-offset/mmap.c
+++ 70_system/mmap__cat-offset/mmap.c
@@ -6,6 +6,49 @@
   ...
 
 
+  mmap()
+
+  The mmap() system call creates a new memory mapping in the calling
+  process's virtual addrss space. A mapping can be of two types:
+
+  - File mapping: A file mapping maps a region of a file directly into
+    the calling process's virtual memory (file-based mapping or
+    memory-based mapping).
+
+  - Anonymous mapping: An anonymous mapping doesn't have a
+    corresponding file. Instead, the pages of the mapping are
+    initialized to 0.
+
+  When two or more processes share the same pages, each process can
+  potentially see the changes to the page contents made by other
+  processes, depending whether the mapping is private or shared.
+
+  - Private mapping (MAP_PRIVATE): Modifications to the contents of
+    the mapping are not visible to other processes and, for a file
+    mapping, are not carried through to the underlying file. A
+    MAP_PRIVATE mapping is sometimes referred to as a private,
+    copy-on-write mapping.
+
+  - Shared mapping (MAP_SHARED): Modifications to the contents of the
+    mapping are visible to other processes that share the same mapping
+    and, for a file mapping, are carried through to the underlying
+    file.
+
+  Purpose of various types of memory mappings
+
+   Visibility of |                     Mapping type
+   modifications |       File              |         Anonymous
+  ---------------+-------------------------+--------------------------
+   Private       | Initializing memory     | Memory allocation
+                 | from contents of file   | exmple: malloc()
+                 | example: copy-on-write  |
+  ---------------+-------------------------+--------------------------
+   Shared        | Memory-mapped I/O;      | Shared memory between
+                 | sharing memory          | processes (IPC)
+                 | between processes (IPC) |
+                 |                         |
+
+
   Demonstrates the use of mmap() to create a private file
   mapping. This program is a simple version of cat. It maps the
   (entire) file named in its command-line argument, and then writes
